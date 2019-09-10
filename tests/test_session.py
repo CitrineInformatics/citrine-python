9c7b6e3332f8b2139039d3e578c21a6eb4d81847
Aris Santas <asantas93@gmail.com>
2019-09-10 10:19:07 -0400

    add TODO

--- tests/test_session.py
+++ tests/test_session.py
@@ -37,6 +37,7 @@ class SessionTests(unittest.TestCase):
 
     @mock.patch.object(Session, '_refresh_access_token')
     def test_extract_response_stacktrace(self, _):
+        # TODO: make this weak test cheat less
         err_json_dict_resp = mock.Mock()
         err_json_dict_resp.json = mock.Mock()
         err_json_dict_resp.json.return_value = {'debug_stacktrace': 'foo'}
