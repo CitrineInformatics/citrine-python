e42585fa928722823a7a4eb3bccc33c1587ea6b8
Brendan Folie <bfolie@citrine.io>
2019-09-09 17:41:55 -0700

    Change FileName link field to url

--- tests/serialization/test_process_spec.py
+++ tests/serialization/test_process_spec.py
@@ -51,7 +51,7 @@ def valid_data():
             'description': 'a long description',
             'tags': []
         },
-        file_links=[{'type': 'file_link', 'filename': 'Cake spec', 'link': 'specs/cake.txt'}],
+        file_links=[{'type': 'file_link', 'filename': 'Cake spec', 'url': 'specs/cake.txt'}],
         type='process_spec'
     )
 
