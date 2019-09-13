14741785c3d832408c2fab527cd07a18a43bbad9
Brendan Folie <bfolie@citrine.io>
2019-09-13 13:26:42 -0700

    Build soft links from process to ingredient specs

--- src/citrine/resources/process_spec.py
+++ src/citrine/resources/process_spec.py
@@ -3,13 +3,16 @@ from typing import Optional, Dict, List, Type
 
 from citrine._utils.functions import set_default_uid
 from citrine._rest.resource import Resource
+from citrine._session import Session
 from citrine._serialization.properties import String, Mapping, Object, LinkOrElse
 from citrine._serialization.properties import List as PropertyList
 from citrine._serialization.properties import Optional as PropertyOptional
 from citrine.resources.data_concepts import DataConcepts, DataConceptsCollection
-from taurus.entity.file_link import FileLink
 from citrine.attributes.condition import Condition
 from citrine.attributes.parameter import Parameter
+from taurus.entity.dict_serializable import DictSerializable
+from taurus.entity.link_by_uid import LinkByUID
+from taurus.entity.file_link import FileLink
 from taurus.entity.object.process_spec import ProcessSpec as TaurusProcessSpec
 from taurus.entity.template.process_template import ProcessTemplate as TaurusProcessTemplate
 
@@ -81,6 +84,64 @@ class ProcessSpec(DataConcepts, Resource['ProcessSpec'], TaurusProcessSpec):
 
     def __str__(self):
         return '<Process spec {!r}>'.format(self.name)
+
+    @classmethod
+    def _build_soft_linked_objects(cls, obj, obj_with_soft_links, session: Session = None):
+        """
+        Build the IngredientSpec objects that this ProcessSpec has soft links to.
+
+        The ingredient specs are found in `obj_with_soft_link`
+
+        This method modifies the object in place.
+
+        Parameters
+        ----------
+        obj: ProcessSpec
+            A ProcessSpec object that might be missing some links to IngredientSpec objects.
+        obj_with_soft_links: dict or \
+        :py:class:`DictSerializable <taurus.entity.dict_serializable.DictSerializable>`
+            A representation of the ProcessSpec in which the IngredientSpecs are encoded.
+            We consider both the possibility that this is a dictionary with an 'ingredients' key
+            and that it is a
+            :py:class:`DictSerializable <taurus.entity.dict_serializable.DictSerializable>`
+            (presumably a
+            :py:class:`TaurusProcessSpec <taurus.entity.process_spec.ProcessSpec>`)
+            with a .ingredients field.
+        session: Session, optional
+            Citrine session used to connect to the database.
+
+        Returns
+        -------
+        None
+            The ProcessSpec object is modified so that it has links to its IngredientSpecs.
+
+        """
+        ingredients = None
+        # Get the ingredients list, if it exists.
+        if isinstance(obj_with_soft_links, dict):
+            if obj_with_soft_links.get('ingredients'):
+                ingredients = obj_with_soft_links['ingredients']
+        if isinstance(obj_with_soft_links, DictSerializable):
+            if hasattr(obj_with_soft_links, 'ingredients'):
+                ingredients = getattr(obj_with_soft_links, 'ingredients')
+        if ingredients is None:
+            return
+
+        from citrine.resources.ingredient_spec import IngredientSpec
+        for ingredient in ingredients:
+            # Cycle through ingredients and if they are not LinkByUID, build them and then
+            # set their `process` field to obj
+            if isinstance(ingredient, DictSerializable):
+                if isinstance(ingredient, LinkByUID):
+                    pass
+                setattr(ingredient, 'process', None)
+            elif isinstance(ingredient, dict):
+                if ingredient.get('type') == LinkByUID.typ:
+                    pass
+                ingredient['process'] = None
+            ingredient_object = IngredientSpec.build(ingredient, session)
+            setattr(ingredient_object, 'process', obj)
+        return
 
 
 class ProcessSpecCollection(DataConceptsCollection[ProcessSpec]):
