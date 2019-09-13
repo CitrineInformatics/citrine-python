68f9ae075a427ba75d96b7eea12693d04a773f6b
Ryan Latture <rlatture@citrine.io>
2019-09-13 12:39:19 -0700

    Added categorical constraint and constraint unit tests.

--- src/citrine/informatics/constraints.py
+++ src/citrine/informatics/constraints.py
@@ -1,10 +1,12 @@
 """Tools for working with Constraints."""
-from typing import Optional
+from typing import List, Optional
 
 from citrine._session import Session
 from citrine._serialization import properties
 from citrine._serialization.serializable import Serializable
 from citrine._serialization.polymorphic_serializable import PolymorphicSerializable
+
+__all__ = ['Constraint', 'ScalarRangeConstraint', 'CategoricalConstraint']
 
 
 class Constraint(PolymorphicSerializable['Constraint']):
@@ -14,8 +16,11 @@ class Constraint(PolymorphicSerializable['Constraint']):
 
     @classmethod
     def get_type(cls, data):
-        """Return the only currently implemented subtype."""
-        return ScalarRangeConstraint
+        """Return the subtype."""
+        return {
+            'ScalarRange': ScalarRangeConstraint,
+            'Categorical': CategoricalConstraint
+        }[data['type']]
 
 
 class ScalarRangeConstraint(Serializable['ScalarRangeConstraint'], Constraint):
@@ -44,3 +49,22 @@ class ScalarRangeConstraint(Serializable['ScalarRangeConstraint'], Constraint):
 
     def __str__(self):
         return '<ScalarRangeConstraint {!r}>'.format(self.descriptor_key)
+
+
+class CategoricalConstraint(Serializable['CategoricalConstraint'], Constraint):
+    """A Citrine CategoricalConstraint."""
+
+    descriptor_key = properties.String('descriptor_key')
+    acceptable_categories = properties.List(properties.String(), 'acceptable_classes')
+    typ = properties.String('type', default='Categorical')
+
+    def __init__(self,
+                 descriptor_key: str,
+                 acceptable_categories: List[str],
+                 session: Optional[Session] = None):
+        self.descriptor_key = descriptor_key
+        self.acceptable_categories = acceptable_categories
+        self.session = session
+
+    def __str__(self):
+        return '<CategoricalConstraint {!r}>'.format(self.descriptor_key)
