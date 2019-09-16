36f367debda41c11e9e93764904283c8302f9b57
Max Gallant <mgallant@citrine.io>
2019-09-16 16:57:57 -0600

    Generalize Generic Response Class
      The response class was specifically expecting two strings for
      instantiation. There is no guarantee that a generic HTTP response
      will have "status" and "message" keys in the body, so this
      relaxes the requirements for instantiating this class.
    
      Since it is used to represent any generic HTTP response we might
      get that may or may not include a response body, this changes
      the class to accept status_code and body (as a dictionary).

--- src/citrine/resources/response.py
+++ src/citrine/resources/response.py
@@ -1,12 +1,25 @@
 class Response:
-    """Model for REST calls that return only responses (e.g. DELETE)."""
+    """Model for REST calls that return only responses (e.g. DELETE). This assumes nothing
+    other than that the response has an HTTP status code attached to it."""
 
-    def __init__(self, message: str, status: str):
-        self.message = message
-        self.status = status
+    def __init__(self, status_code: int = None, body: dict = None):
+        self.status_code = status_code
+        self.body = body
+
+    def _get_status_string(self):
+        if self.status_code is not None:
+            return str(self.status_code)
+        else:
+            return "No HTTP status available"
+
+    def _get_body_string(self):
+        if self.body is None:
+            return str(self.body)
+        else:
+            return "No body available"
 
     def __repr__(self):
-        return 'Response({!r}, {!r})'.format(self.message, self.status)
+        return 'Response({!r}), {!r})'.format(self._get_status_string(), self._get_body_string())
 
     def __str__(self):
-        return '<Response {!r}>'.format(self.message)
+        return '<Response {!r}>'.format(self._get_status_string())
