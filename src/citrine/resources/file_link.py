176d58d87f90060a054e89b1259b8fa51b895b00
Brendan Folie <bfolie@citrine.io>
2019-09-16 20:24:24 -0700

    Break file upload out into three parts

--- src/citrine/resources/file_link.py
+++ src/citrine/resources/file_link.py
@@ -12,6 +12,21 @@ from citrine._rest.resource import Resource
 from citrine._session import Session
 
 
+class _Uploader(object):
+    """Holds the many parameters that are generated and used during file upload."""
+
+    def __init__(self):
+        self.bucket = ''
+        self.key = ''
+        self.upload_id = ''
+        self.region_name = ''
+        self.aws_access_key_id = ''
+        self.aws_secret_access_token = ''
+        self.aws_secret_access_key = ''
+        self.aws_session_token = ''
+        self.object_key = ''
+
+
 class FileLink(Resource['FileLink'], TaurusFileLink):
     """Resource that stores the name and url of an external file."""
 
@@ -41,7 +56,7 @@ class FileCollection(Collection[FileLink]):
         """Build an instance of FileLink."""
         return FileLink.build(data)
 
-    def upload(self, file_path, dest_name=None) -> FileLink:
+    def upload(self, file_path: str, dest_name: str = None) -> FileLink:
         """
         Uploads a file to the dataset.
 
@@ -67,6 +82,29 @@ class FileCollection(Collection[FileLink]):
             # Use the file name as a default dest_name
             dest_name = os.path.basename(file_path)
 
+        uploader = self._make_upload_request(file_path, dest_name)
+        uploader = self._upload_file(file_path, uploader)
+        return self._complete_upload(dest_name, uploader)
+
+    def _make_upload_request(self, file_path: str, dest_name: str):
+        """
+        Make a request to the backend to upload a file.
+
+        Parameters
+        ----------
+        file_path: str
+            The path to the file on the local computer.
+        dest_name: str
+            The name the file will have after being uploaded.
+
+        Returns
+        -------
+        _Uploader
+            Holds the parameters returned by the upload request, for later use.
+            These must include region_name, aws_access_key_id, aws_secret_access_key,
+            aws_session_token, bucket, object_key, & upload_id.
+
+        """
         path = self._get_path() + "/uploads"
         extension = os.path.splitext(file_path)[1]
         mimeType = mimetypes.types_map[extension]
@@ -82,35 +120,80 @@ class FileCollection(Collection[FileLink]):
         }
         # POST request creates space in S3 for the file and returns AWS-related information
         # (such as temporary credentials) that allow the file to be uploaded.
-        upload_response = self.session.post_resource(path=path, json=upload_json)
+        upload_request = self.session.post_resource(path=path, json=upload_json)
+        uploader = _Uploader()
 
-        # Extract all relevant information from the POST response
+        # Extract all relevant information from the upload request
         try:
-            region_name = upload_response['s3_region']
-            aws_access_key_id = upload_response['temporary_credentials']['access_key_id']
-            aws_secret_access_key = upload_response['temporary_credentials']['secret_access_key']
-            aws_session_token = upload_response['temporary_credentials']['session_token']
-            bucket = upload_response['s3_bucket']
-            object_key = upload_response['uploads'][0]['s3_key']
-            upload_id = upload_response['uploads'][0]['upload_id']
+            uploader.region_name = upload_request['s3_region']
+            uploader.aws_access_key_id = upload_request['temporary_credentials']['access_key_id']
+            uploader.aws_secret_access_key = \
+                upload_request['temporary_credentials']['secret_access_key']
+            uploader.aws_session_token = upload_request['temporary_credentials']['session_token']
+            uploader.bucket = upload_request['s3_bucket']
+            uploader.object_key = upload_request['uploads'][0]['s3_key']
+            uploader.upload_id = upload_request['uploads'][0]['upload_id']
         except KeyError:
             raise RuntimeError("Upload initiation response is missing some fields: "
-                               "{}".format(upload_response))
+                               "{}".format(upload_request))
+        return uploader
 
+    @staticmethod
+    def _upload_file(file_path: str, uploader: _Uploader):
+        """
+        Upload a file to S3.
+
+        Parameters
+        ----------
+        file_path: str
+            The path to the file on the local computer.
+        uploader: _Uploader
+            Holds the parameters returned by the upload request.
+
+        Returns
+        -------
+        _Uploader
+            The input uploader object with its s3_version field now populated.
+
+        """
         s3_client = boto3_client('s3',
-                                 region_name=region_name,
-                                 aws_access_key_id=aws_access_key_id,
-                                 aws_secret_access_key=aws_secret_access_key,
-                                 aws_session_token=aws_session_token)
+                                 region_name=uploader.region_name,
+                                 aws_access_key_id=uploader.aws_access_key_id,
+                                 aws_secret_access_key=uploader.aws_secret_access_key,
+                                 aws_session_token=uploader.aws_session_token)
         with open(file_path, 'rb') as f:
             try:
-                upload_s3_response = s3_client.put_object(Bucket=bucket, Key=object_key, Body=f)
+                upload_response = s3_client.put_object(
+                    Bucket=uploader.bucket,
+                    Key=uploader.object_key,
+                    Body=f,
+                    Metadata={"X-Citrine-Upload-Id": uploader.upload_id})
             except ClientError as e:
                 raise RuntimeError("Upload of file {} failed with the following "
                                    "exception: {}".format(file_path, e))
-            s3_version = upload_s3_response['VersionId']
-            path = self._get_path() + "/uploads/{}/complete".format(upload_id)
-            complete_response = self.session.put_resource(path=path, json={'s3_version': s3_version})
+        uploader.s3_version = upload_response['VersionId']
+        return uploader
+
+    def _complete_upload(self, dest_name: str, uploader: _Uploader):
+        """
+        Indicate that the upload has finished and determine the file URL.
+
+        Parameters
+        ----------
+        dest_name: str
+            The name the file will have after being uploaded.
+        uploader: _Uploader
+            Holds the parameters returned by the upload request and the upload response.
+
+        Returns
+        -------
+        FileLink
+            The filename and url of the uploaded object.
+
+        """
+        path = self._get_path() + "/uploads/{}/complete".format(uploader.upload_id)
+        complete_response = self.session.put_resource(path=path,
+                                                      json={'s3_version': uploader.s3_version})
 
         try:
             file_id = complete_response['file_info']['file_id']
