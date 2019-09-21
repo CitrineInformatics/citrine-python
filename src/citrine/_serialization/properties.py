0df94f38ddc2a0fe40200823940436ed39dcce90
Justin Harris <jharris@citrine.io>
2019-09-21 16:28:26 -0600

    Adding more property and function unit tests

--- src/citrine/_serialization/properties.py
+++ src/citrine/_serialization/properties.py
@@ -130,13 +130,13 @@ class Integer(Property[int, SerializedInteger]):
 
     def _deserialize(self, value: SerializedInteger) -> int:
         if isinstance(value, bool):
-            raise AttributeError('value must be a Number, not a boolean.')
+            raise TypeError('value must be a Number, not a boolean.')
         else:
             return int(value)
 
     def _serialize(self, value: int) -> SerializedInteger:
         if isinstance(value, bool):
-            raise AttributeError('Boolean cannot be serialized to integer.')
+            raise TypeError('Boolean cannot be serialized to integer.')
         else:
             return value
 
@@ -164,6 +164,9 @@ class Float(Property[float, SerializedFloat]):
     @classmethod
     def _serialize(cls, value: float) -> SerializedFloat:
         return value
+
+    def __str__(self):
+        return '<Float {!r}>'.format(self.serialization_path)
 
 
 class Raw(Property[typing.Any, typing.Any]):
@@ -263,7 +266,7 @@ class Datetime(Property[datetime, int]):
         if isinstance(value, int):
             # Backend returns time as ms since epoch, but arrow expects seconds since epoch
             return arrow.get(value / 1000).datetime
-        raise ValueError("{} must be an int or a string".format(value))
+        raise TypeError("{} must be an int or a string".format(value))
 
     def _serialize(self, value: datetime) -> int:
         return int(arrow.get(value).float_timestamp * 1000)
--- tests/_serialization/test_simple_properties.py
+++ tests/_serialization/test_simple_properties.py
@@ -1,6 +1,8 @@
 import pytest
+import arrow
 
-from ._data import VALID_SERIALIZATIONS, INVALID_INSTANCES, INVALID_SERIALIZED_INSTANCES
+from citrine._serialization.properties import Integer, Property, String, Float, Datetime
+from ._data import VALID_SERIALIZATIONS, VALID_STRINGS, INVALID_INSTANCES, INVALID_SERIALIZED_INSTANCES
 
 
 @pytest.mark.parametrize('prop_type,value,serialized', VALID_SERIALIZATIONS)
@@ -24,3 +26,53 @@ def test_invalid_property_deserialization(prop_type, serialized):
         prop.deserialize(serialized)
 
 
+@pytest.mark.parametrize('prop_type,path,expected', VALID_STRINGS)
+def test_invalid_property_deserialization(prop_type, path, expected):
+    assert expected == str(prop_type(path))
+
+
+def test_serialize_to_dict_error():
+    with pytest.raises(ValueError):
+        Integer().serialize_to_dict({}, 1)
+
+
+def test_valid_serialize_to_dict():
+    assert {'my_foo': 100} == Integer('my_foo').serialize_to_dict({}, 100)
+
+
+def test_serialize_dot_value_to_dict():
+    assert {'my': {'foo': 100}} == Integer('my.foo').serialize_to_dict({}, 100)
+
+
+def test_set_int_property_from_string():
+    class Foo:
+        bar = Integer('bar')
+
+    f = Foo()
+    f.bar = '12'
+
+    assert 12 == f.bar
+
+
+def test_string_property_deserialize_none():
+    with pytest.raises(ValueError):
+        String()._deserialize(None)
+
+
+def test_int_cannot_deserialize_bool():
+    with pytest.raises(TypeError):
+        Integer()._deserialize(False)
+
+
+def test_float_cannot_deserialize_bool():
+    with pytest.raises(TypeError):
+        Float()._deserialize(False)
+
+
+def test_deserialize_string_datetime():
+    assert arrow.get('2019-07-19T10:46:08+00:00').datetime == Datetime().deserialize('2019-07-19T10:46:08+00:00')
+
+
+def test_datetime_cannot_deserialize_float():
+    with pytest.raises(TypeError):
+        Datetime()._deserialize(1.114)
