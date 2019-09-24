02825a83a65573ea6029aff97dc0406a3b19efdb
Sean Paradiso <seanparadiso@gmail.com>
2019-09-23 23:42:30 -0700

    Revert "Merge branch 'feature/orion' into develop"
    This reverts commit 4e4506be44b269af22b6a6e3ceb2419a9d98952b, reversing
    changes made to 3c943db4e73d67fe786cc633ae2cc9c2c7b49864.

--- src/citrine/exceptions.py
+++ src/citrine/exceptions.py
@@ -29,7 +29,7 @@ class NotFound(NonRetryableException):
     """A particular url was not found. (http status 404)."""
 
     def __init__(self, path: str):
-        super().__init__(path)
+        super().__init__()
         self.url = path
 
 
@@ -37,17 +37,5 @@ class Unauthorized(NonRetryableException):
     """The user is unauthorized to make this api call. (http status 401)."""
 
     def __init__(self, path: str):
-        super().__init__(path)
+        super().__init__()
         self.url = path
-
-
-class WorkflowConflictException(NonRetryableException):
-    """There is a conflict preventing the workflow from being executed. (http status 409)."""
-
-    pass
-
-
-class WorkflowNotReadyException(RetryableException):
-    """The workflow is not ready to be executed. i.e. still validating. (http status 425)."""
-
-    pass
--- src/citrine/informatics/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tools for working with Citrine Informatics functionality."""
