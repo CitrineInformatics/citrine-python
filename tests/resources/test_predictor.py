c73be73e5cc3a4e56637922fd4988df78bf40344
Ryan Latture <rlatture@citrine.io>
2019-10-08 13:23:21 -0700

    Removed paraboloid predictor.

--- tests/resources/test_predictor.py
+++ tests/resources/test_predictor.py
@@ -4,32 +4,43 @@ import pytest
 import uuid
 
 from citrine.exceptions import ModuleRegistrationFailedException, NotFound
-from citrine.informatics.predictors import ParaboloidPredictor
+from citrine.informatics.predictors import SimpleMLPredictor
 from citrine.resources.predictor import PredictorCollection
-from ..serialization.test_predictors import valid_paraboloid_data, valid_simple_ml_data
+from ..serialization.test_predictors import valid_simple_ml_data
 
 
-def test_build(valid_paraboloid_data):
-    pc = PredictorCollection(uuid.uuid4(), mock.Mock())
-    predictor = pc.build(valid_paraboloid_data)
-    assert predictor.name == 'my predictor'
-    assert predictor.description == 'does some things'
-
-
-def test_register(valid_paraboloid_data):
+def test_build(valid_simple_ml_data):
     session = mock.Mock()
-    session.post_resource.return_value = valid_paraboloid_data
+    session.get_resource.return_value = {
+        'id': str(uuid.uuid4()),
+        'status': 'VALID',
+        'report': {}
+    }
     pc = PredictorCollection(uuid.uuid4(), session)
-    predictor = ParaboloidPredictor.build(valid_paraboloid_data)
+    predictor = pc.build(valid_simple_ml_data)
+    assert predictor.name == 'ML predictor'
+    assert predictor.description == 'Predicts z from input x and latent variable y'
+
+
+def test_register(valid_simple_ml_data):
+    session = mock.Mock()
+    session.post_resource.return_value = valid_simple_ml_data
+    session.get_resource.return_value = {
+        'id': str(uuid.uuid4()),
+        'status': 'VALID',
+        'report': {}
+    }
+    pc = PredictorCollection(uuid.uuid4(), session)
+    predictor = SimpleMLPredictor.build(valid_simple_ml_data)
     registered = pc.register(predictor)
-    assert registered.name == 'my predictor'
+    assert registered.name == 'ML predictor'
 
 
-def test_failed_register(valid_paraboloid_data):
+def test_failed_register(valid_simple_ml_data):
     session = mock.Mock()
     session.post_resource.side_effect = NotFound("/projects/uuid/not_found")
     pc = PredictorCollection(uuid.uuid4(), session)
-    predictor = ParaboloidPredictor.build(valid_paraboloid_data)
+    predictor = SimpleMLPredictor.build(valid_simple_ml_data)
     with pytest.raises(ModuleRegistrationFailedException) as e:
         pc.register(predictor)
-    assert 'The "ParaboloidPredictor" failed to register. NotFound: /projects/uuid/not_found' in str(e.value)
+    assert 'The "SimpleMLPredictor" failed to register. NotFound: /projects/uuid/not_found' in str(e.value)
