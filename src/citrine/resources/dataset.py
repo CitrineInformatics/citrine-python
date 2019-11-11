bc870b89d27e394a53f47b414b7431778e1efe32
Brendan Folie <bfolie@citrine.io>
2019-11-11 12:45:28 -0800

    Remove unused imports

--- src/citrine/resources/dataset.py
+++ src/citrine/resources/dataset.py
@@ -1,6 +1,5 @@
 """Resources that represent both individual and collections of datasets."""
 from uuid import UUID
-from typing import Union, Any
 
 from citrine._session import Session
 from citrine._rest.collection import Collection
@@ -22,7 +21,6 @@ from citrine.resources.material_spec import MaterialSpecCollection
 from citrine.resources.ingredient_run import IngredientRunCollection
 from citrine.resources.ingredient_spec import IngredientSpecCollection
 from citrine.resources.file_link import FileCollection
-from citrine.resources.response import Response
 
 
 class Dataset(Resource['Dataset']):
