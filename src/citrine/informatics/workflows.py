1743657029e678f2e6263802694e90e4c9e17a52
Kyle Laplante <klaplante@citrine.io>
2019-12-04 19:25:08 -0600

    performance workflows

--- src/citrine/informatics/workflows.py
+++ src/citrine/informatics/workflows.py
@@ -7,6 +7,7 @@ from citrine._serialization import properties
 from citrine._rest.resource import Resource
 from citrine._session import Session
 from citrine.resources.workflow_executions import WorkflowExecutionCollection
+from citrine.informatics.analysis_configuration import CrossValidationAnalysisConfiguration
 
 __all__ = ['Workflow', 'DesignWorkflow']
 
@@ -23,8 +24,20 @@ class Workflow(PolymorphicSerializable['Workflow']):
 
     @classmethod
     def get_type(cls, data) -> Type['Workflow']:
-        """Return the sole currently implemented subtype."""
-        return DesignWorkflow
+        """Return the subtype."""
+        type_dict = {
+            'DESIGN_WORKFLOW': DesignWorkflow,
+            'PERFORMANCE_WORKFLOW': PerformanceWorkflow,
+        }
+        typ = type_dict.get(data['module_type'])
+
+        if typ is not None:
+            return typ
+        else:
+            raise ValueError(
+                '{} is not a valid workflow type. '
+                'Must be in {}.'.format(data['module_type'], type_dict.keys())
+            )
 
 
 class DesignWorkflow(Resource['DesignWorkflow'], Workflow):
@@ -54,15 +67,12 @@ class DesignWorkflow(Resource['DesignWorkflow'], Workflow):
         serializable=False
     )
     active = properties.Boolean('active', default=True)
+    design_space_id = properties.UUID('config.design_space_id')
+    processor_id = properties.UUID('config.processor_id')
+    predictor_id = properties.UUID('config.predictor_id')
+    module_type = properties.String('module_type', default='DESIGN_WORKFLOW')
+    schema_id = properties.UUID('schema_id', default=UUID('8af8b007-3e81-4185-82b2-6f62f4a2e6f1'))
 
-    # TODO: Figure out how to make these fields richer/use actual objects
-    design_space_id = properties.UUID('modules.design_space_id')
-    processor_id = properties.UUID('modules.processor_id')
-    predictor_id = properties.UUID('modules.predictor_id')
-
-    # The project_id is used to keep a reference to the project under which the workflow was
-    # created. It is currently unclear if this is the best way to do this. Another option might
-    # be to have all objects have a context object, but that also seems to have downsides.
     def __init__(self,
                  name: str,
                  design_space_id: UUID,
@@ -79,6 +89,53 @@ class DesignWorkflow(Resource['DesignWorkflow'], Workflow):
 
     def __str__(self):
         return '<DesignWorkflow {!r}>'.format(self.name)
+
+    @property
+    def executions(self) -> WorkflowExecutionCollection:
+        """Return a resource representing all visible executions of this workflow."""
+        if getattr(self, 'project_id', None) is None:
+            raise AttributeError('Cannot initialize execution without project reference!')
+        return WorkflowExecutionCollection(self.project_id, self.uid, self.session)
+
+
+class PerformanceWorkflow(Resource['PerformanceWorkflow'], Workflow):
+    """Object that executes performance analysis on a given module.
+
+    Parameters
+    ----------
+    name: str
+        the name of the workflow
+    analysis: CrossValidationAnalysisConfiguration
+        the configuration object
+
+    """
+
+    uid = properties.Optional(properties.UUID, 'id', serializable=False)
+    name = properties.String('display_name')
+    status = properties.String('status', serializable=False)
+    status_info = properties.Optional(
+        properties.List(properties.String()),
+        'status_info',
+        serializable=False
+    )
+    active = properties.Boolean('active', default=True)
+    analysis = properties.Object(CrossValidationAnalysisConfiguration, 'config.analysis')
+    module_type = properties.String('module_type', default='PERFORMANCE_WORKFLOW')
+    schema_id = properties.UUID('schema_id', default=UUID('1d213f0a-d07c-4f70-a4d0-bda3aa951ee0'))
+    typ = properties.String('config.type', default='PerformanceWorkflow', deserializable=False)
+
+    def __init__(self,
+                 name: str,
+                 analysis: CrossValidationAnalysisConfiguration,
+                 project_id: Optional[UUID] = None,
+                 session: Session = Session()):
+        self.name = name
+        self.analysis = analysis
+        self.project_id = project_id
+        self.session = session
+
+    def __str__(self):
+        return '<PerformanceWorkflow {!r}>'.format(self.name)
 
     @property
     def executions(self) -> WorkflowExecutionCollection:
