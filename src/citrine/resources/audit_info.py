fab48d2382044c83a822777dbfaf8bd26327124c
Aris Santas <asantas93@gmail.com>
2019-12-05 20:48:34 -0500

    correct outdated audit info type hints

--- src/citrine/resources/audit_info.py
+++ src/citrine/resources/audit_info.py
@@ -12,13 +12,13 @@ class AuditInfo(Serializable):
 
     Parameters
     ----------
-    created_by: str
+    created_by: Optional[UUID]
         ID of the user who created the object
-    created_at: int
+    created_at: Optional[Datetime]
         Time, in ms since epoch, at which the object was created
-    updated_by: Optional[str]
+    updated_by: Optional[UUID]
         ID of the user who most recently updated the object
-    updated_at: Optional[int]
+    updated_at: Optional[Datetime]
         Time, in ms since epoch, at which the object was most recently updated
 
     """
