78ea6eba218f9cb0367d915daf3f73a6575a509e
Chris LeBlanc <crleblanc@gmail.com>
2020-01-07 18:40:36 +1300

    Proposed bugfix for PLA-2416: using pagination for listing collections

--- src/citrine/_rest/collection.py
+++ src/citrine/_rest/collection.py
@@ -13,6 +13,8 @@ ResourceType = TypeVar('ResourceType', bound='Resource')
 # have subclasses override the create method.
 CreationType = TypeVar('CreationType', bound='Resource')
 
+DEFAULT_PER_PAGE = 20
+
 
 class Collection(Generic[ResourceType]):
     """Abstract class for representing collections of REST resources."""
@@ -53,11 +55,14 @@ class Collection(Generic[ResourceType]):
         except NonRetryableException as e:
             raise ModuleRegistrationFailedException(model.__class__.__name__, e)
 
-    def list(self,
-             page: Optional[int] = None,
-             per_page: Optional[int] = None) -> Iterable[ResourceType]:
+    def _fetch_page(self,
+                    page: Optional[int] = None,
+                    per_page: Optional[int] = None) -> Iterable[ResourceType]:
         """
-        List all visible elements in the collection.
+        Fetch visible elements in the collection.  This does not handle pagination.
+        This method will return the first page of results using the default page/per_page
+        behaviour of the backend service.  Specify page/per_page to override these defaults
+        which are passed to the backend service.
 
         Parameters
         ---------
@@ -97,6 +102,70 @@ class Collection(Generic[ResourceType]):
                 # Module collections are not filtering on module type
                 # properly, so we are filtering client-side.
                 pass
+
+    def list(self,
+             page: Optional[int] = None,
+             per_page: Optional[int] = None) -> Iterable[ResourceType]:
+        """
+        List all visible elements in the collection.  Leaving page and per_page as
+        default values will yield all elements in the collection, paginating over
+        all available pages.
+
+        Parameters
+        ---------
+        page: int, optional
+            The "page" of results to list. Default is to read all pages and yield
+            all results.
+        per_page: int, optional
+            Max number of results to return per page. Default is 20.  If the page
+            parameter is specified this will limit the number of results in that
+            response.
+
+        Returns
+        -------
+        Iterable[ResourceType]
+            Resources in this collection.
+
+        """
+
+        # Do an initial fetch of the first page using supplied args.  If more results expected, proceed with paginated
+        # calls
+        first_page = self._fetch_page(page=page, per_page=per_page)
+        first_page_count = 0
+        first_uid = None
+        for idx, element in enumerate(first_page):
+            yield element
+
+            uid = getattr(element, 'uid', None)
+            if idx == 0 and not first_uid and uid:
+                first_uid = uid
+
+            first_page_count += 1
+
+        if page is not None or first_page_count != (per_page or DEFAULT_PER_PAGE):
+            return
+
+        # Read extra pages until nothing is left to read
+        next_page = 2
+        while True:
+            subset = self._fetch_page(page=next_page, per_page=per_page)
+            count = 0
+            for idx, element in enumerate(subset):
+                # escaping from infinite loops where page/per_page are not honored and are returning the
+                # same results regardless of page:
+                uid = getattr(element, 'uid', None)
+                if first_uid is not None and first_uid == uid:
+                    break
+
+                yield element
+
+                count += 1
+
+            # Handle the case where we get an unexpected number of results (e.g. last page)
+            if count == 0 or count < first_page_count:
+                break
+
+            next_page += 1
 
     def update(self, model: CreationType) -> CreationType:
         """Update a particular element of the collection."""
--- tests/resources/test_dataset.py
+++ tests/resources/test_dataset.py
@@ -3,7 +3,7 @@ import pytest
 
 from citrine.resources.dataset import DatasetCollection
 from tests.utils.factories import DatasetDataFactory, DatasetFactory
-from tests.utils.session import FakeSession, FakeCall
+from tests.utils.session import FakeSession, FakePaginatedSession, FakeCall
 
 
 @pytest.fixture
@@ -12,10 +12,23 @@ def session() -> FakeSession:
 
 
 @pytest.fixture
+def paginated_session() -> FakePaginatedSession:
+    return FakePaginatedSession()
+
+
+@pytest.fixture
 def collection(session) -> DatasetCollection:
     return DatasetCollection(
         project_id=UUID('6b608f78-e341-422c-8076-35adc8828545'),
         session=session
+    )
+
+
+@pytest.fixture
+def paginated_collection(paginated_session) -> DatasetCollection:
+    return DatasetCollection(
+        project_id=UUID('6b608f78-e341-422c-8076-35adc8828545'),
+        session=paginated_session
     )
 
 
@@ -48,19 +61,26 @@ def test_register_dataset(collection, session):
     assert name == dataset.name
 
 
-def test_list_datasets(collection, session):
+def test_list_datasets(paginated_collection, paginated_session):
     # Given
-    datasets_data = DatasetDataFactory.create_batch(5)
-    session.set_response(datasets_data)
+    datasets_data = DatasetDataFactory.create_batch(50)
+    paginated_session.set_response(datasets_data)
 
     # When
-    datasets = list(collection.list())
+    datasets = list(paginated_collection.list())
 
     # Then
-    assert 1 == session.num_calls
-    expected_call = FakeCall(method='GET', path='projects/{}/datasets'.format(collection.project_id))
-    assert expected_call == session.last_call
-    assert 5 == len(datasets)
+    assert 3 == paginated_session.num_calls
+    expected_first_call = FakeCall(method='GET', path='projects/{}/datasets'.format(paginated_collection.project_id))
+    expected_last_call = FakeCall(method='GET', path='projects/{}/datasets'.format(paginated_collection.project_id),
+                                  params={'page': 3})
+    assert expected_first_call == paginated_session.calls[0]
+    assert expected_last_call == paginated_session.last_call
+    assert 50 == len(datasets)
+
+    expected_uids = [str(d.uid) for d in datasets]
+    dataset_ids = [d['id'] for d in datasets_data]
+    assert expected_uids == dataset_ids
 
 
 def test_delete_dataset(collection, session, dataset):
