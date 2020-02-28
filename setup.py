f6a5c75a7a1433184fad294051c2068a16db922b
Chris LeBlanc <crleblanc@gmail.com>
2020-02-28 13:37:05 +1300

    Implimented a build_py command to strip hints before building the wheel
    This is performed for all versions of Python 3.  This leaves function
    and method type hints in place so most of the user benefit remains
    unchanged.

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 from setuptools.command.develop import develop
+from setuptools.command.build_py import build_py
 import sys
-import subprocess
+from os.path import join
+from os import walk
+
+STRIP_DIRS = ["src", "tests"]
+
+
+def strip_file(file_path):
+    from strip_hints import strip_file_to_string
+
+    if file_path.endswith('.py'):
+        stripped = strip_file_to_string(file_path, to_empty=True, only_assigns_and_defs=True)
+        print(file_path)
+        with open(file_path, 'w') as file_obj:
+            file_obj.write(stripped)
+
+
+def strip_all_hints(dirnames):
+    print('stripping type annotations from: {}'.format(dirnames))
+    for dirname in dirnames:
+        for root, dirs, files in walk(dirname):
+            for file_name in files:
+                strip_file(join(root, file_name))
 
 
 class PostInstallCommand(install):
@@ -11,8 +33,7 @@ class PostInstallCommand(install):
         """Python version is constrained to [3.5, 4.0), so if the minor version is < 6,
         run the script to strip type hints"""
         if sys.version_info.minor < 6:
-            subprocess.call('chmod 755 scripts/strip_hints.sh', shell=True)
-            subprocess.call('./scripts/strip_hints.sh', shell=True)
+            strip_all_hints(STRIP_DIRS)
         install.run(self)
 
 
@@ -22,9 +43,17 @@ class PostDevelopCommand(develop):
         """Python version is constrained to [3.5, 4.0), so if the minor version is < 6,
         run the script to strip type hints"""
         if sys.version_info.minor < 6:
-            subprocess.call('chmod 755 scripts/strip_hints.sh', shell=True)
-            subprocess.call('./scripts/strip_hints.sh', shell=True)
+            strip_all_hints(STRIP_DIRS)
         develop.run(self)
+
+
+class PreBuildCommand(build_py):
+    """Pre-build command for build_py"""
+    def run(self):
+        """Strip out all variable declaration type hints since we create a single wheel file
+        for all versions of Python 3 and Python 3.5 does not support these hints"""
+        strip_all_hints(STRIP_DIRS)
+        build_py.run(self)
 
 
 setup(name='citrine',
@@ -39,7 +68,7 @@ setup(name='citrine',
           "requests>=2.22.0,<3",
           "pyjwt>=1.7.1,<2",
           "arrow>=0.15.4,<0.16",
-          "strip-hints>=0.1.5,<=0.1.7",
+          "strip-hints>=0.1.8",
           "taurus-citrine>=0.6.0,<0.7",
           "boto3>=1.9.226,<2",
           "botocore>=1.12.226,<2",
@@ -47,6 +76,7 @@ setup(name='citrine',
       ],
       cmdclass={
           'install': PostInstallCommand,
-          'develop': PostDevelopCommand
+          'develop': PostDevelopCommand,
+          'build_py': PreBuildCommand,
       }
 )
