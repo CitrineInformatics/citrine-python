75cbc2ca635df4504f1f8dab3733b53c3dac86e8
bfolie <bfolie@citrine.io>
2020-03-16 15:48:36 -0400

    Copy graph predictor data because it is modified during build

--- tests/serialization/test_predictors.py
+++ tests/serialization/test_predictors.py
@@ -1,6 +1,7 @@
 """Tests for citrine.informatics.predictors serialization."""
 import pytest
 from uuid import UUID
+from copy import deepcopy
 
 from citrine.informatics.predictors import ExpressionPredictor, GraphPredictor, Predictor, SimpleMLPredictor
 from citrine.informatics.descriptors import RealDescriptor
@@ -49,11 +50,12 @@ def test_legacy_serialization(valid_simple_ml_predictor_data):
 
 def test_graph_serialization(valid_graph_predictor_data):
     """Ensure that a serialized GraphPredictor looks sane."""
+    graph_data_copy = deepcopy(valid_graph_predictor_data)
     predictor = GraphPredictor.build(valid_graph_predictor_data)
     serialized = predictor.dump()
-    serialized['id'] = valid_graph_predictor_data['id']
-    assert serialized['config']['predictors'] == valid_graph_predictor_data['config']['predictors']
-    assert serialized == valid_serialization_output(valid_graph_predictor_data)
+    serialized['id'] = graph_data_copy['id']
+    assert serialized['config']['predictors'] == graph_data_copy['config']['predictors']
+    assert serialized == valid_serialization_output(graph_data_copy)
 
 
 def test_expression_serialization(valid_expression_predictor_data):
