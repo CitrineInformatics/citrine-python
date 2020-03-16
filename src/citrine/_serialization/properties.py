45abb51f0da75bdca6ae3c748bf6a0642f0e0c0f
bfolie <bfolie@citrine.io>
2020-03-16 14:55:32 -0400

    Add debugging print during Union deser

--- src/citrine/_serialization/properties.py
+++ src/citrine/_serialization/properties.py
@@ -408,7 +408,8 @@ class Union(Property[typing.Any, typing.Any]):
         for prop in self.element_types:
             try:
                 return prop.deserialize(value)
-            except ValueError:
+            except ValueError as e:
+                print(e)
                 pass
         raise RuntimeError("An unexpected error occurred while trying to deserialize {} to one "
                            "of the following types: {}.".format(value, self.underlying_types))
