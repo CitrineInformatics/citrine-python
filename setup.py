ec15e32b64d7078ea3d479ca6ae57e6e46d14c28
Dana Katzenelson <dkatzenelson@citrine.io>
2020-04-02 13:26:36 -0700

    Add validation that the version has been increased compared to master
    Add bumping version to PR template
    
    Bump version (also reflecting prior changes)

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.9.1',
+      version='0.10.0',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Andrew Millspaugh',
