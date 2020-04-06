655c0c07f19b75aa3f8c46d59fb4691b73cd539b
Aris Santas <asantas93@gmail.com>
2020-04-06 11:55:24 -0400

    minor version bump

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.11.0',
+      version='0.12.0',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Andrew Millspaugh',
