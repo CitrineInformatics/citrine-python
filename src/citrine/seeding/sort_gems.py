7636457eede542a5f1313aeb5e2a0194e77d8456
Dana Katzenelson <dkatzenelson@citrine.io>
2020-04-07 10:48:50 -0700

    Add seeding utility for sorting templating from data objects

---
+++ src/citrine/seeding/sort_gems.py
@@ -0,0 +1,22 @@
+from taurus.entity.template.attribute_template import AttributeTemplate
+from taurus.entity.template.base_template import BaseTemplate
+
+
+def sort_templates_from_objects(gems):
+    """
+    Sort the provided gems into two lists.
+
+    The first list contains only templates (both ObjectTemplate and AttributeTemplate).
+    The second list contains only data objects (specs and runs).
+
+    Useful when the procedure for seeding templates needs to be distinct from the procedure for
+    seeding data objects.
+    """
+    templates = []
+    data_objects = []
+    for gem in gems:
+        if isinstance(gem, AttributeTemplate) or isinstance(gem, BaseTemplate):
+            templates.append(gem)
+        else:
+            data_objects.append(gem)
+    return templates, data_objects
