97e7d17e302e6ee47451342a8b4f136a7717606a
Brendan Folie <bfolie@citrine.io>
2020-04-13 14:11:54 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.15.4',
+      version='0.15.5',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Andrew Millspaugh',
