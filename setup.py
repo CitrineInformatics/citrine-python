1513ac2da7dceb7e2597a39874b59aae7c2d2108
Aris Santas <asantas93@gmail.com>
2020-04-22 15:04:13 -0400

    Fix type hints which were too broad

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.16.5',
+      version='0.16.6',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Andrew Millspaugh',
