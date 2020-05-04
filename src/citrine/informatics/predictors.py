27fb56368ecbd23c574af3f3c05000eae74f1254
Ryan Latture <rlatture@citrine.io>
2020-05-04 07:51:34 -0700

    Added formulation predictors.

--- src/citrine/informatics/predictors.py
+++ src/citrine/informatics/predictors.py
@@ -1,19 +1,19 @@
 """Tools for working with Predictors."""
 from abc import abstractmethod
-from typing import List, Optional, Type, Union
+from typing import Dict, List, Optional, Type, Union
 from uuid import UUID
 
-from citrine._serialization import properties
+from citrine._serialization import properties as _properties
 from citrine._serialization.serializable import Serializable
 from citrine._session import Session
 from citrine.informatics.data_sources import DataSource
-from citrine.informatics.descriptors import Descriptor
+from citrine.informatics.descriptors import Descriptor, FormulationDescriptor, RealDescriptor
 from citrine.informatics.reports import Report
 from citrine.resources.report import ReportResource
 from citrine.informatics.modules import Module
 
-
-__all__ = ['ExpressionPredictor', 'GraphPredictor', 'Predictor', 'SimpleMLPredictor']
+__all__ = ['ExpressionPredictor', 'GraphPredictor', 'IngredientsToSimpleMixturePredictor', 'Predictor',
+           'SimpleMLPredictor']
 
 
 class Predictor(Module):
@@ -37,6 +37,8 @@ class Predictor(Module):
             "Simple": SimpleMLPredictor,
             "Graph": GraphPredictor,
             "Expression": ExpressionPredictor,
+            "IngredientsToSimpleMixture": IngredientsToSimpleMixturePredictor,
+            "GeneralizedMeanProperty": GeneralizedMeanPropertyPredictor,
         }
         typ = type_dict.get(data['config']['type'])
 
@@ -73,25 +75,25 @@ class SimpleMLPredictor(Serializable['SimplePredictor'], Predictor):
 
     """
 
-    uid = properties.Optional(properties.UUID, 'id', serializable=False)
-    name = properties.String('config.name')
-    description = properties.Optional(properties.String(), 'config.description')
-    inputs = properties.List(properties.Object(Descriptor), 'config.inputs')
-    outputs = properties.List(properties.Object(Descriptor), 'config.outputs')
-    latent_variables = properties.List(properties.Object(Descriptor), 'config.latent_variables')
-    training_data = properties.Object(DataSource, 'config.training_data')
-    typ = properties.String('config.type', default='Simple', deserializable=False)
-    status = properties.Optional(properties.String(), 'status', serializable=False)
-    status_info = properties.Optional(
-        properties.List(properties.String()),
+    uid = _properties.Optional(_properties.UUID, 'id', serializable=False)
+    name = _properties.String('config.name')
+    description = _properties.Optional(_properties.String(), 'config.description')
+    inputs = _properties.List(_properties.Object(Descriptor), 'config.inputs')
+    outputs = _properties.List(_properties.Object(Descriptor), 'config.outputs')
+    latent_variables = _properties.List(_properties.Object(Descriptor), 'config.latent_variables')
+    training_data = _properties.Object(DataSource, 'config.training_data')
+    typ = _properties.String('config.type', default='Simple', deserializable=False)
+    status = _properties.Optional(_properties.String(), 'status', serializable=False)
+    status_info = _properties.Optional(
+        _properties.List(_properties.String()),
         'status_info',
         serializable=False
     )
-    active = properties.Boolean('active', default=True)
+    active = _properties.Boolean('active', default=True)
 
     # NOTE: These could go here or in _post_dump - it's unclear which is better right now
-    module_type = properties.String('module_type', default='PREDICTOR')
-    schema_id = properties.UUID('schema_id', default=UUID('08d20e5f-e329-4de0-a90a-4b5e36b91703'))
+    module_type = _properties.String('module_type', default='PREDICTOR')
+    schema_id = _properties.UUID('schema_id', default=UUID('08d20e5f-e329-4de0-a90a-4b5e36b91703'))
 
     def __init__(self,
                  name: str,
@@ -139,25 +141,25 @@ class GraphPredictor(Serializable['GraphPredictor'], Predictor):
 
     """
 
-    uid = properties.Optional(properties.UUID, 'id', serializable=False)
-    name = properties.String('config.name')
-    description = properties.String('config.description')
-    predictors = properties.List(properties.Union(
-        [properties.UUID, properties.Object(Predictor)]), 'config.predictors')
-    typ = properties.String('config.type', default='Graph', deserializable=False)
+    uid = _properties.Optional(_properties.UUID, 'id', serializable=False)
+    name = _properties.String('config.name')
+    description = _properties.String('config.description')
+    predictors = _properties.List(_properties.Union(
+        [_properties.UUID, _properties.Object(Predictor)]), 'config.predictors')
+    typ = _properties.String('config.type', default='Graph', deserializable=False)
     # Graph predictors may not be embedded in other predictors, hence while status is optional
     # for deserializing most predictors, it is required for deserializing a graph
-    status = properties.String('status', serializable=False)
-    status_info = properties.Optional(
-        properties.List(properties.String()),
+    status = _properties.String('status', serializable=False)
+    status_info = _properties.Optional(
+        _properties.List(_properties.String()),
         'status_info',
         serializable=False
     )
-    active = properties.Boolean('active', default=True)
+    active = _properties.Boolean('active', default=True)
 
     # NOTE: These could go here or in _post_dump - it's unclear which is better right now
-    module_type = properties.String('module_type', default='PREDICTOR')
-    schema_id = properties.UUID('schema_id', default=UUID('43c61ad4-7e33-45d0-a3de-504acb4e0737'))
+    module_type = _properties.String('module_type', default='PREDICTOR')
+    schema_id = _properties.UUID('schema_id', default=UUID('43c61ad4-7e33-45d0-a3de-504acb4e0737'))
 
     def __init__(self,
                  name: str,
@@ -225,24 +227,24 @@ class ExpressionPredictor(Serializable['ExpressionPredictor'], Predictor):
 
     """
 
-    uid = properties.Optional(properties.UUID, 'id', serializable=False)
-    name = properties.String('config.name')
-    description = properties.String('config.description')
-    expression = properties.String('config.expression')
-    output = properties.Object(Descriptor, 'config.output')
-    aliases = properties.Mapping(properties.String, properties.String, 'config.aliases')
-    typ = properties.String('config.type', default='Expression', deserializable=False)
-    status = properties.Optional(properties.String(), 'status', serializable=False)
-    status_info = properties.Optional(
-        properties.List(properties.String()),
+    uid = _properties.Optional(_properties.UUID, 'id', serializable=False)
+    name = _properties.String('config.name')
+    description = _properties.String('config.description')
+    expression = _properties.String('config.expression')
+    output = _properties.Object(Descriptor, 'config.output')
+    aliases = _properties.Mapping(_properties.String, _properties.String, 'config.aliases')
+    typ = _properties.String('config.type', default='Expression', deserializable=False)
+    status = _properties.Optional(_properties.String(), 'status', serializable=False)
+    status_info = _properties.Optional(
+        _properties.List(_properties.String()),
         'status_info',
         serializable=False
     )
-    active = properties.Boolean('active', default=True)
+    active = _properties.Boolean('active', default=True)
 
     # NOTE: These could go here or in _post_dump - it's unclear which is better right now
-    module_type = properties.String('module_type', default='PREDICTOR')
-    schema_id = properties.UUID('schema_id', default=UUID('866e72a6-0a01-4c5f-8c35-146eb2540166'))
+    module_type = _properties.String('module_type', default='PREDICTOR')
+    schema_id = _properties.UUID('schema_id', default=UUID('866e72a6-0a01-4c5f-8c35-146eb2540166'))
 
     def __init__(self,
                  name: str,
@@ -268,6 +270,163 @@ class ExpressionPredictor(Serializable['ExpressionPredictor'], Predictor):
 
     def __str__(self):
         return '<ExpressionPredictor {!r}>'.format(self.name)
+
+    def post_build(self, project_id: UUID, data: dict):
+        """Creates the predictor report object."""
+        self.report = ReportResource(project_id, self.session).get(data['id'])
+
+
+class IngredientsToSimpleMixturePredictor(Serializable['IngredientsToSimpleMixturePredictor'], Predictor):
+    """[ALPHA] A predictor interface that constructs a simple mixture from ingredient quantities.
+
+    Parameters
+    ----------
+    name: str
+        name of the configuration
+    description: str
+        description of the predictor
+    output: FormulationDescriptor
+        descriptor that represents the output formulation
+    id_to_quantity: Dict[str, RealDescriptor]
+        Map from ingredient identifier to the descriptor that represents its quantity,
+        e.g. ``{'water': RealDescriptor('water quantity', 0, 1)}``
+    labels: Dict[str, List[str]]
+        Map from each label to all components assigned that label, when present in a mixture,
+        e.g. ``{'solvent': ['water']}``
+
+    """
+
+    uid = _properties.Optional(_properties.UUID, 'id', serializable=False)
+    name = _properties.String('config.name')
+    description = _properties.String('config.description')
+    output = _properties.Object(FormulationDescriptor, 'config.output')
+    id_to_quantity = _properties.Mapping(_properties.String, _properties.Object(RealDescriptor),
+                                         'config.id_to_quantity')
+    labels = _properties.Mapping(_properties.String, _properties.List(_properties.String), 'config.labels')
+    typ = _properties.String('config.type', default='IngredientsToSimpleMixture', deserializable=False)
+    status = _properties.Optional(_properties.String(), 'status', serializable=False)
+    status_info = _properties.Optional(
+        _properties.List(_properties.String()),
+        'status_info',
+        serializable=False
+    )
+    active = _properties.Boolean('active', default=True)
+
+    # NOTE: These could go here or in _post_dump - it's unclear which is better right now
+    module_type = _properties.String('module_type', default='PREDICTOR')
+    schema_id = _properties.UUID('schema_id', default=UUID('873e4541-da8a-4698-a981-732c0c729c3d'))
+
+    def __init__(self,
+                 name: str,
+                 description: str,
+                 output: FormulationDescriptor,
+                 id_to_quantity: Dict[str, RealDescriptor],
+                 labels: Dict[str, List[str]],
+                 session: Optional[Session] = None,
+                 report: Optional[Report] = None,
+                 active: bool = True):
+        self.name: str = name
+        self.description: str = description
+        self.output: FormulationDescriptor = output
+        self.id_to_quantity: Dict[str, RealDescriptor] = id_to_quantity
+        self.labels: Dict[str, List[str]] = labels
+        self.session: Optional[Session] = session
+        self.report: Optional[Report] = report
+        self.active: bool = active
+
+    def _post_dump(self, data: dict) -> dict:
+        data['display_name'] = data['config']['name']
+        return data
+
+    def __str__(self):
+        return '<IngredientsToSimpleMixturePredictor {!r}>'.format(self.name)
+
+    def post_build(self, project_id: UUID, data: dict):
+        """Creates the predictor report object."""
+        self.report = ReportResource(project_id, self.session).get(data['id'])
+
+
+class GeneralizedMeanPropertyPredictor(Serializable['GeneralizedMeanPropertyPredictor'], Predictor):
+    """[ALPHA] A predictor interface that mean component properties.
+
+    Parameters
+    ----------
+    name: str
+        name of the configuration
+    description: str
+        description of the predictor
+    input_descriptor: FormulationDescriptor
+        descriptor that represents the input formulation
+    properties: List[str]
+        List of component properties to featurize
+    p: float
+        Power of the generalized mean
+    impute_properties: bool
+        Whether to impute missing component properties
+    training_data: DataSource
+        Source of the training data, which can be either a CSV or an Ara table
+    label: Optional[str]
+        Optional label
+    default_properties: Optional[Dict[str, float]]
+        Default values to use for imputed properties. ``impute_properties`` must be ``True``
+        Defaults are specified as a map from descriptor key to its default value.
+
+    """
+
+    uid = _properties.Optional(_properties.UUID, 'id', serializable=False)
+    name = _properties.String('config.name')
+    description = _properties.String('config.description')
+    input_descriptor = _properties.Object(FormulationDescriptor, 'config.input')
+    properties = _properties.List(_properties.String, 'config.properties')
+    p = _properties.Float('config.p')
+    impute__properties = _properties.Boolean('config.impute_properties')
+    training_data = _properties.Object(DataSource, 'config.training_data')
+    default__properties = _properties.Optional(_properties.Mapping(_properties.String, _properties.Float),
+                                               'config.default_properties')
+    label = _properties.Optional(_properties.String, 'config.label')
+    typ = _properties.String('config.type', default='GeneralizedMeanProperty', deserializable=False)
+    status = _properties.Optional(_properties.String(), 'status', serializable=False)
+    status_info = _properties.Optional(
+        _properties.List(_properties.String()),
+        'status_info',
+        serializable=False
+    )
+    active = _properties.Boolean('active', default=True)
+
+    # NOTE: These could go here or in _post_dump - it's unclear which is better right now
+    module_type = _properties.String('module_type', default='PREDICTOR')
+    schema_id = _properties.UUID('schema_id', default=UUID('29e53222-3217-4f81-b3b8-4197a8211ade'))
+
+    def __init__(self,
+                 name: str,
+                 description: str,
+                 input_descriptor: FormulationDescriptor,
+                 properties: List[str],
+                 impute_properties: bool,
+                 training_data: DataSource,
+                 default_properties: Optional[Dict[str, float]] = None,
+                 label: Optional[str] = None,
+                 session: Optional[Session] = None,
+                 report: Optional[Report] = None,
+                 active: bool = True):
+        self.name: str = name
+        self.description: str = description
+        self.input_descriptor: FormulationDescriptor = input_descriptor
+        self.properties: List[str] = properties
+        self.impute_properties: bool = impute_properties
+        self.training_data = training_data
+        self.default_properties: Optional[Dict[str, float]] = default_properties
+        self.label: Optional[str] = label
+        self.session: Optional[Session] = session
+        self.report: Optional[Report] = report
+        self.active: bool = active
+
+    def _post_dump(self, data: dict) -> dict:
+        data['display_name'] = data['config']['name']
+        return data
+
+    def __str__(self):
+        return '<GeneralizedMeanPropertyPredictor {!r}>'.format(self.name)
 
     def post_build(self, project_id: UUID, data: dict):
         """Creates the predictor report object."""
--- tests/informatics/test_predictors.py
+++ tests/informatics/test_predictors.py
@@ -4,13 +4,18 @@ import pytest
 import uuid
 
 from citrine.informatics.data_sources import AraTableDataSource
-from citrine.informatics.descriptors import RealDescriptor
-from citrine.informatics.predictors import ExpressionPredictor, GraphPredictor, SimpleMLPredictor
+from citrine.informatics.descriptors import RealDescriptor, FormulationDescriptor
+from citrine.informatics.predictors import ExpressionPredictor, GeneralizedMeanPropertyPredictor, GraphPredictor, \
+    SimpleMLPredictor, IngredientsToSimpleMixturePredictor
 
 x = RealDescriptor("x", 0, 100, "")
 y = RealDescriptor("y", 0, 100, "")
 z = RealDescriptor("z", 0, 100, "")
 shear_modulus = RealDescriptor('Property~Shear modulus', lower_bound=0, upper_bound=100, units='GPa')
+formulation = FormulationDescriptor('formulation')
+water_quantity = RealDescriptor('water quantity', 0, 1)
+salt_quantity = RealDescriptor('salt quantity', 0, 1)
+data_source = AraTableDataSource(uuid.UUID('e5c51369-8e71-4ec6-b027-1f92bdc14762'), 0)
 
 
 @pytest.fixture
@@ -21,7 +26,7 @@ def simple_predictor() -> SimpleMLPredictor:
                              inputs=[x],
                              outputs=[z],
                              latent_variables=[y],
-                             training_data=AraTableDataSource(uuid.UUID('e5c51369-8e71-4ec6-b027-1f92bdc14762'), 0))
+                             training_data=data_source)
 
 
 @pytest.fixture
@@ -38,14 +43,47 @@ def expression_predictor() -> ExpressionPredictor:
         description='Computes shear modulus from Youngs modulus and Poissons ratio',
         expression='Y / (2 * (1 + v))',
         output=shear_modulus,
-        aliases = {
-             'Y': "Property~Young's modulus",
-             'v': "Property~Poisson's ratio"
+        aliases={
+            'Y': "Property~Young's modulus",
+            'v': "Property~Poisson's ratio"
         })
 
 
+@pytest.fixture
+def ing_to_simple_mixture_predictor() -> IngredientsToSimpleMixturePredictor:
+    """Build an IngredientsToSimpleMixturePredictor for testing."""
+    return IngredientsToSimpleMixturePredictor(
+        name='Ingredients to simple mixture predictor',
+        description='Constructs a mixture from ingredient quantities',
+        output=formulation,
+        id_to_quantity={
+            'water': water_quantity,
+            'salt': salt_quantity
+        },
+        labels={
+            'solvent': ['water'],
+            'solute': ['salt']
+        }
+    )
+
+
+@pytest.fixture
+def generalized_mean_property_predictor() -> GeneralizedMeanPropertyPredictor:
+    """Build a mean property predictor for testing."""
+    return GeneralizedMeanPropertyPredictor(
+        name='Mean property predictor',
+        description='Computes mean component properties',
+        input_descriptor=formulation,
+        properties=['density'],
+        impute_properties=True,
+        training_data=data_source,
+        default_properties={'density': 1.0},
+        label='solvent'
+    )
+
+
 def test_simple_initialization(simple_predictor):
-    """Make sure the correct fields go to the correct places for the Simple Predictor."""
+    """Make sure the correct fields go to the correct places for a simple predictor."""
     assert simple_predictor.name == 'ML predictor'
     assert simple_predictor.description == 'Predicts z from input x and latent variable y'
     assert len(simple_predictor.inputs) == 1
@@ -72,7 +110,7 @@ def test_simple_post_build(simple_predictor):
 
 
 def test_graph_initialization(graph_predictor):
-    """Make sure the correct fields go to the correct places for the Graph Predictor."""
+    """Make sure the correct fields go to the correct places for a graph predictor."""
     assert graph_predictor.name == 'Graph predictor'
     assert graph_predictor.description == 'description'
     assert len(graph_predictor.predictors) == 2
@@ -92,7 +130,7 @@ def test_graph_post_build(graph_predictor):
 
 
 def test_expression_initialization(expression_predictor):
-    """Make sure the correct fields go to the correct places for the Expression Predictor."""
+    """Make sure the correct fields go to the correct places for an expression predictor."""
     assert expression_predictor.name == 'Expression predictor'
     assert expression_predictor.output.key == 'Property~Shear modulus'
     assert expression_predictor.expression == 'Y / (2 * (1 + v))'
@@ -101,7 +139,7 @@ def test_expression_initialization(expression_predictor):
 
 
 def test_expression_post_build(expression_predictor):
-    """Ensures we get a report from a expression predictor post_build call."""
+    """Ensures we get a report from an expression predictor post_build call."""
     assert expression_predictor.report is None
     session = mock.Mock()
     session.get_resource.return_value = dict(status='OK', report=dict(), uid=uuid.uuid4())
@@ -110,3 +148,50 @@ def test_expression_post_build(expression_predictor):
     assert session.get_resource.call_count == 1
     assert expression_predictor.report is not None
     assert expression_predictor.report.status == 'OK'
+
+
+def test_ing_to_simple_mixture_initialization(ing_to_simple_mixture_predictor):
+    """Make sure the correct fields go to the correct places for an ingredients to simple mixture predictor."""
+    assert ing_to_simple_mixture_predictor.name == 'Ingredients to simple mixture predictor'
+    assert ing_to_simple_mixture_predictor.output.key == 'formulation'
+    assert ing_to_simple_mixture_predictor.id_to_quantity == {'water': water_quantity, 'salt': salt_quantity}
+    assert ing_to_simple_mixture_predictor.labels == {'solvent': ['water'], 'solute': ['salt']}
+    expected_str = '<IngredientsToSimpleMixturePredictor \'Ingredients to simple mixture predictor\'>'
+    assert str(ing_to_simple_mixture_predictor) == expected_str
+
+
+def test_ing_to_simple_mixture_post_build(ing_to_simple_mixture_predictor):
+    """Ensures we get a report from an ingredients to simple mixture predictor post_build call."""
+    assert ing_to_simple_mixture_predictor.report is None
+    session = mock.Mock()
+    session.get_resource.return_value = dict(status='OK', report=dict(), uid=uuid.uuid4())
+    ing_to_simple_mixture_predictor.session = session
+    ing_to_simple_mixture_predictor.post_build(uuid.uuid4(), dict(id=uuid.uuid4()))
+    assert session.get_resource.call_count == 1
+    assert ing_to_simple_mixture_predictor.report is not None
+    assert ing_to_simple_mixture_predictor.report.status == 'OK'
+
+
+def test_generalized_mean_property_initialization(generalized_mean_property_predictor):
+    """Make sure the correct fields go to the correct places for a mean property predictor."""
+    assert generalized_mean_property_predictor.name == 'Mean property predictor'
+    assert generalized_mean_property_predictor.input_descriptor.key == 'formulation'
+    assert generalized_mean_property_predictor.properties == ['density']
+    assert generalized_mean_property_predictor.impute_properties == True
+    assert generalized_mean_property_predictor.training_data == data_source
+    assert generalized_mean_property_predictor.default_properties == {'density': 1.0}
+    assert generalized_mean_property_predictor.label == 'solvent'
+    expected_str = '<GeneralizedMeanPropertyPredictor \'Mean property predictor\'>'
+    assert str(generalized_mean_property_predictor) == expected_str
+
+
+def test_generalized_mean_property_post_build(generalized_mean_property_predictor):
+    """Ensures we get a report from a mean property predictor post_build call."""
+    assert generalized_mean_property_predictor.report is None
+    session = mock.Mock()
+    session.get_resource.return_value = dict(status='OK', report=dict(), uid=uuid.uuid4())
+    generalized_mean_property_predictor.session = session
+    generalized_mean_property_predictor.post_build(uuid.uuid4(), dict(id=uuid.uuid4()))
+    assert session.get_resource.call_count == 1
+    assert generalized_mean_property_predictor.report is not None
+    assert generalized_mean_property_predictor.report.status == 'OK'
