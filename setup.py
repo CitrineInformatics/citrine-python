449bd23adab995b1f1f5d0efbd6362703f360244
Ken Kroenlein <kkroenlein@citrine.io>
2020-06-02 17:57:02 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.33.0',
+      version='0.34.0',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Citrine Informatics',
