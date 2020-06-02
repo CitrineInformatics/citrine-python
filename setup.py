6e80420d50721b2188d3c5216158532cfc95a35c
Chris LeBlanc <crleblanc@gmail.com>
2020-06-02 15:41:51 -0700

    bumped version due to slight API change

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.32.1',
+      version='0.33.0',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Citrine Informatics',
