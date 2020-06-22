b54b8a247c647c59ebffb5b9aa54604843473dca
Dave LeBlanc <david.leblanc@gmail.com>
2020-06-22 16:20:25 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.39',
+      version='0.39.0',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Citrine Informatics',
