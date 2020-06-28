dcd60513b2379b450b83ee5055d94f7992a3e4ae
Nils Persson <npersson@citrine.io>
2020-06-28 16:31:45 -0400

    add design_space builders tests

--- src/citrine/builders/design_spaces.py
+++ src/citrine/builders/design_spaces.py
@@ -1,32 +1,51 @@
 import pandas as pd
 from itertools import product
-from typing import Mapping, Sequence, Any
+from typing import Mapping, Sequence, Any, List
 
+from citrine.informatics.design_spaces import EnumeratedDesignSpace
+from citrine.informatics.descriptors import Descriptor
 
 def enumerate_cartesian_product(
-    dof_grids: Mapping[str, Sequence],
-) -> Sequence[Mapping[str, Any]]:
-    '''[ALPHA] Given a dict of lists or tuples of values for each degree of freedom,
+    design_grids: Mapping[str, Sequence],
+    descriptors: List[Descriptor],
+    name: str = 'Enumerated Cartesian product design space',
+    description: str = '',
+) -> EnumeratedDesignSpace:
+    """[ALPHA] Given a dict of lists or tuples of values for each design descriptor,
     create the list of candidates representing the Cartesian product of all
-    values for each degree of freedom
+    values for each descriptor
 
     Parameters
     ----------
-    dof_grids: Mapping[str, Sequence]
-        {'degree of freedom':[sequence, of, values]} for each DoF
-    '''
-    design_space_tuples = list(product(*dof_grids.values()))
-    design_space_cols = list(dof_grids.keys())
+    design_grids: Mapping[str, Sequence]
+        {'descriptor key': [sequence, of, values]} for each descriptor
+    descriptors: List[Descriptor]
+        design descriptors representing the degrees of freedom of the design
+        space. descriptors' keys should match the keys of design_grids
+    name: str
+        name for the EnumeratedDesignSpace
+    description: str
+        description for the EnumeratedDesignSpace
+    """    
+    design_space_tuples = list(product(*design_grids.values()))
+    design_space_cols = list(design_grids.keys())
     df_ds = pd.DataFrame(data=design_space_tuples, columns=design_space_cols)
-    candidates = df_ds.to_dict('records')
-    return candidates
+    data = df_ds.to_dict('records')
+    design_space = EnumeratedDesignSpace(name=name,
+                                         description=description,
+                                         descriptors=descriptors,
+                                         data=data)
+    return design_space
 
 
 def enumerate_simple_mixture_cartesian_product(
     formulation_grids: Mapping[str, Sequence[float]],
     balance_component: str,
-) -> Sequence[Mapping[str, float]]:
-    '''[ALPHA] Given a dict of lists or tuples of ingredient amounts (must be
+    descriptors: List[Descriptor],
+    name: str = 'Enumerated Cartesian product simple mixture design space',
+    description: str = '',
+) -> EnumeratedDesignSpace:
+    """[ALPHA] Given a dict of lists or tuples of ingredient amounts (must be
     fractions of some kind, i.e. between 0-1), create candidates that are
     the Cartesian product of all possible combinations of ingredients *except*
     for the balance_component. The balance_component will then be filled in
@@ -34,7 +53,7 @@ def enumerate_simple_mixture_cartesian_product(
     
     The balance component's [list, of, values] can be similar to the other
     components'; in this way, the balance component can be swapped out easily.
-    However, the balance component's discretization will be ignored - only
+    However, the balance component's intermediate values will be ignored - only
     its minimum and maximum values will be considered. Mixtures in which
     the balance component amount falls outside its min and max values will be
     filtered out of the design space. In other words, formulation_grids could
@@ -55,19 +74,39 @@ def enumerate_simple_mixture_cartesian_product(
     balance_component: str
         name of the component that should make up the balance of the
         mixture (1-others). Must be a key in formulation_grids.
-    '''
+    descriptors: List[Descriptor]
+        design descriptors representing the degrees of freedom of the design
+        space. descriptors' keys should match the keys of formulation_grids
+    name: str
+        name for the EnumeratedDesignSpace
+    description: str
+        description for the EnumeratedDesignSpace
+    """
 
+    # Check that the passed balance component is in the grid keys
     assert balance_component in list(formulation_grids.keys()), \
         "balance component must be in formulation_grids' keys"
     non_balance_comps = [comp for comp in list(formulation_grids.keys())
                          if comp != balance_component]
+    non_balance_descriptors = [dd for dd in descriptors 
+                               if dd.key != balance_component]
+    non_balance_grids = {
+        k: v for k, v in formulation_grids.items()
+        if k in non_balance_comps
+        }
+    
+    # Check that all grid values are between 0 and 1
+    for kk, val_list in formulation_grids.items():
+        assert all([x>=0 for x in val_list]) and all([x<=1 for x in val_list]), \
+            f'values in {kk} are not between 0 and 1'
 
     # Start by making a naive product design space of non-balance components
+
     form_ds = pd.DataFrame(
         enumerate_cartesian_product(
-            {k: v for k, v in formulation_grids.items()
-             if k in non_balance_comps}
-        )
+            design_grids=non_balance_grids,
+            descriptors=non_balance_descriptors
+        ).data
     )
     
     # Re-calculate the balance component column as 1-all others
@@ -87,36 +126,57 @@ def enumerate_simple_mixture_cartesian_product(
     form_ds = form_ds[form_ds[balance_component] >= balance_range[0]]
     form_ds = form_ds[form_ds[balance_component] <= balance_range[1]]
     data = form_ds.to_dict('records')
-
-    return data
+    design_space = EnumeratedDesignSpace(name=name,
+                                         description=description,
+                                         descriptors=descriptors,
+                                         data=data)
+    return design_space
 
 
 def cartesian_join_design_spaces(
-    data_list: Sequence[Sequence[Mapping[str, Any]]],
-) -> Sequence[Mapping[str, Any]]:
-    ''' Given multiple input design spaces (lists of candidates), create a
-    set of candidates that is the Cartesian product of candidates from each
+    design_space_list: List[EnumeratedDesignSpace],
+    name: str = 'Joined enumerated design space',
+    description: str = '',
+) -> EnumeratedDesignSpace:
+    """[ALPHA] Given a list of multiple input EnumeratedDesignSpaces, create a new
+    EnumeratedDesignSpace that is the Cartesian product of candidates from each
     input design space.
     
-    ultimate length will be len(data_list[0])*len(data_list[2])*...
+    Ultimate length will be the product of the number of candidates in each
+    individual design space
     
     Parameters
     ----------
-    data_list: Sequence[Sequence[Mapping[str, Any]]]
-        A list of [lists of candidates] (data for individual design subspaces)
-    '''
+    design_space_list: List[EnumeratedDesignSpace]
+        A list of EnumeratedDesignSpaces
+    """
     
-    assert 'join_key' not in [data[0].keys() for data in data_list], \
+    assert 'join_key' not in [ds.data[0].keys() for ds in design_space_list], \
         '"join_key" cannot be a descriptor key when using this function'
     
+    # Convert data fields of EDS into DataFrames to prep for join
+    data_list = [ds.data for ds in design_space_list]
     ds_list = [pd.DataFrame(data) for data in data_list]
     
+    # Set a dummy column to do the join
     for df in ds_list:
         df['join_key'] = 0
     
+    # Perform the join
     df_join = ds_list[0]
     for df in ds_list[1:]:
         df_join = pd.merge(df_join, df, on='join_key')
+        
+    # Drop the dummy column and convert back to List[dict]
     df_join = df_join.drop(columns='join_key')
     data = df_join.to_dict('records')
-    return data
+    
+    # Build the final DesignSpace
+    descriptors = []
+    for ds in design_space_list:
+        descriptors.extend(ds.descriptors)
+    design_space = EnumeratedDesignSpace(name=name,
+                                         description=description,
+                                         descriptors=descriptors,
+                                         data=data)
+    return design_space
---
+++ tests/builders/test_design_spaces.py
@@ -0,0 +1,74 @@
+"""Tests for citrine.informatics.design_spaces."""
+import pytest
+
+from citrine.informatics.descriptors import RealDescriptor, CategoricalDescriptor
+from citrine.informatics.design_spaces import EnumeratedDesignSpace
+from citrine.builders.design_spaces import *
+
+@pytest.fixture
+def basic_cartesian_space() -> EnumeratedDesignSpace:
+    """Build basic cartesian space for testing."""
+    alpha = RealDescriptor('alpha', 0, 100)
+    beta = RealDescriptor('beta', 0, 100)
+    gamma = CategoricalDescriptor('gamma', ['a', 'b', 'c'])
+    design_grids = {
+        'alpha': [0, 50, 100],
+        'beta': [0, 25, 50, 75, 100],
+        'gamma': ['a','b','c']
+    }
+    basic_space = enumerate_cartesian_product(
+        design_grids=design_grids,
+        descriptors=[alpha, beta, gamma],
+        name='basic space',
+        description=''
+    )
+    return basic_space
+
+
+@pytest.fixture
+def simple_mixture_space() -> EnumeratedDesignSpace:
+    """Build simple mixture space for testing."""
+    ing_A = RealDescriptor('ing_A', 0, 1)
+    ing_B = RealDescriptor('ing_B', 0, 1)
+    ing_C = RealDescriptor('ing_C', 0, 1)
+    formulation_grids = {
+        'ing_A': [0.6, 0.7, 0.8, 0.9, 1.0],
+        'ing_B': [0, 0.1, 0.2, 0.3, 0.4],
+        'ing_C': [0.0, 0.01, 0.02, 0.03, 0.04, 0.05]
+    }
+    simple_mixture_space = enumerate_simple_mixture_cartesian_product(
+        formulation_grids=formulation_grids,
+        balance_component='ing_A',
+        descriptors=[ing_A, ing_B, ing_C],
+        name='basic simple mixture space',
+        description=''
+    )
+    return simple_mixture_space
+
+
+@pytest.fixture
+def joint_design_space() -> EnumeratedDesignSpace:
+    """Build joint design space from above two examples"""
+    ds_list = [basic_cartesian_space(), simple_mixture_space()]
+    joint_space = cartesian_join_design_spaces(
+        design_space_list=ds_list,
+        name='Joined enumerated design space',
+        description='',
+    )
+    return joint_space
+
+
+def test_enumerated_cartesian(basic_cartesian_space):
+    """Check data length"""
+    assert len(basic_cartesian_space.data) == 45
+
+
+def test_simple_mixture(simple_mixture_space):
+    """Check data length"""
+    assert len(simple_mixture_space.data) == 25
+    
+    
+def test_joined(joint_design_space):
+    """Check data length and number of descriptors"""
+    assert len(joint_design_space.data) == 1125
+    assert len(joint_design_space.descriptors) == 6
