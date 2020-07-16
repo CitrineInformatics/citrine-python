eee0e2d23fa61e3125e1be82101e98884d55e16d
Zoe Siff Yang <zyang@citrine.io>
2020-07-16 14:07:17 -0700

    modify project search_params to not include search_params key

--- src/citrine/resources/project.py
+++ src/citrine/resources/project.py
@@ -392,19 +392,17 @@ class ProjectCollection(Collection[Project]):
             A dict representing the body of the post request that will be sent to the search
             endpoint to filter the results ie.
             {
-                "search_params": {
-                    "name": {
-                        "value": "Polymer Project",
-                        "search_method": "EXACT"
-                    },
-                    "description": {
-                        "value": "polymer chain length",
-                        "search_method": "SUBSTRING"
-                    },
-                }
+                "name": {
+                    "value": "Polymer Project",
+                    "search_method": "EXACT"
+                },
+                "description": {
+                    "value": "polymer chain length",
+                    "search_method": "SUBSTRING"
+                },
             }
             The dict can constain any combination of (one or all) search specifications for the
-            name, description, and status fields of a project. For each parameter specified, the
+        name, description, and status fields of a project. For each parameter specified, the
             "value" to match, as well as the "search_method" must be provided. The available
             search_methods are "SUBSTRING" and "EXACT". The example above demonstrates the input
             necessary to list projects with the exact name "Polymer Project," and descriptions
@@ -454,7 +452,7 @@ class ProjectCollection(Collection[Project]):
             field should be passed as the key for the outermost dict, with its value the request
             body, so that we can easily unpack the keyword argument when it gets passed to
             fetch_func.
-            ie. { 'search_params': {'name': {'value': 'Project', 'search_method': 'SUBSTRING'} } }
+            ie. {'name': {'value': 'Project', 'search_method': 'SUBSTRING'} }
 
         Returns
         -------
@@ -466,7 +464,7 @@ class ProjectCollection(Collection[Project]):
         """
         # Making 'json' the key of the outermost dict, so that search_params can be passed
         # directly to the function making the request with keyword expansion
-        json_body = {} if search_params is None else {'json': search_params}
+        json_body = {} if search_params is None else {'json': {'search_params': search_params}}
 
         path = self._get_path() + "/search"
 
