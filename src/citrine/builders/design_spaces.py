Fix calculation in cartesian_join_design_spaces

src/citrine/builders/design_spaces.py
@@ -200,11 +200,11 @@ def cartesian_join_design_spaces(
         raise ValueError('Duplicate keys are not allowed across design spaces')
 
     # Check that the grid size is small enough to not cause memory issues
-    grid_size = np.prod([len(ds.data) * len(ds.data[0]) for ds in subspaces])
+    grid_size = np.prod([len(ds.data[0]) for ds in subspaces]) \
+        * np.sum([len(ds.data) for ds in subspaces])
     if grid_size > 2e8:
         warn("Product design grid contains {n} grid points. This may cause memory issues "
-             "downstream."
-             .format(n=grid_size))
+             "downstream.".format(n=grid_size))
 
     # Convert data fields of EDS into DataFrames to prep for join
     ds_list = [pd.DataFrame(ds.data) for ds in subspaces]