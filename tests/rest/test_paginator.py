d679a658ea3d7907c0b8bf81a69e0006e7c69366
Max Hutchinson <maxhutch@gmail.com>
2020-09-03 09:21:12 -0400

    Deduplicate list results by their uid
    Since we're doing pagination-based listing, creates and deletes
    to the collection being listed can result in index movement during
    a multi-request list operation.  In particular, the repetition of
    a list element from the end of one page to the beginning of the next
    has been observed to occur when items are added to the list.  This
    change keeps a list of the uids that have been encountered and skips
    returning duplicates. If the listed object isn't a resource and
    therefore doesn't have a uid, then no deduplication is performed.
    
    This behavior can be toggled via a flag to assist future developers.

--- tests/rest/test_paginator.py
+++ tests/rest/test_paginator.py
@@ -1,4 +1,6 @@
 """Test the Paginator"""
+from uuid import uuid4
+
 from mock import Mock
 
 from citrine._rest.paginator import Paginator
@@ -6,29 +8,32 @@ from citrine._rest.paginator import Paginator
 
 def test_validate_mock_fetcher():
     """Just ensure our mock fetcher method does the right thing for what the paginator expects"""
-    mock_fetcher = mocked_fetcher("a", "b")
+    mock_fetcher = mocked_fetcher(DummyResource("a"), DummyResource("b"))
 
     # Make calls (and ignore arguments on the callable), and ensure we get back the above elements, each wrapped in a
     # list, and the final call should be an empty list.
-    assert mock_fetcher("a", "b") == (["a"], "next_uri")
-    assert mock_fetcher() == (["b"], "next_uri")
+    assert mock_fetcher(DummyResource("a"), DummyResource("b")) == ([DummyResource("a")], "next_uri")
+    assert mock_fetcher() == ([DummyResource("b")], "next_uri")
     assert mock_fetcher() == ([], "")
 
 
-def test_paginator_fetches_single_page():
-    # Return a single page with 2 elements
-    result = Paginator().paginate(mocked_fetcher(["a", "b"]), lambda x: x, per_page=2)
-    assert list(result) == ["a", "b"]
-
-
 def test_pagination_across_two_pages():
-    result = Paginator().paginate(mocked_fetcher("a", "b"), lambda x: x, per_page=1)
-    assert list(result) == ["a", "b"]
+    result = Paginator().paginate(mocked_fetcher(DummyResource("a"), DummyResource("b")), lambda x: x, per_page=1)
+    assert list(result) == [DummyResource("a"), DummyResource("b")]
 
 
 def test_pagination_stops_when_initial_item_repeated():
-    result = Paginator().paginate(mocked_fetcher("a", "b", "a"), lambda x: x, per_page=1)
-    assert list(result) == ["a", "b"]
+    a = DummyResource("a")
+    result = Paginator().paginate(mocked_fetcher(a, DummyResource("b"), a), lambda x: x, per_page=1)
+    assert list(result) == [a, DummyResource("b")]
+
+
+def test_pagination_deduplicates_repeated_intermediate_values():
+    a = DummyResource("a")
+    b = DummyResource("b")
+    c = DummyResource("c")
+    result = Paginator().paginate(mocked_fetcher(a, b, b, b, b, b, b, c, c), lambda x: x, per_page=1)
+    assert list(result) == [a, b, c]
 
 
 def mocked_fetcher(*args):
@@ -39,8 +44,21 @@ def mocked_fetcher(*args):
     This lets us mock a fetcher easily.
     """
     mock_fetcher = Mock()
-    args_in_lists = [list(a) for a in args]
+    args_in_lists = [[a] for a in args]
     args_in_lists = [(a, "next_uri") for a in args_in_lists]
     args_in_lists.append(([], ""))
     mock_fetcher.side_effect = list(args_in_lists)
     return mock_fetcher
+
+
+class DummyResource(object):
+    """
+    Dummy resource that stores a string val
+    """
+
+    def __init__(self, val: str):
+        self.val = val
+        self.uid = uuid4()
+
+    def __eq__(self, other):
+        return self.val == other.val
