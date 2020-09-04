2a8f7a7055c924fc3f93b76c9551a47c07bcbded
Stephen Edkins <sedkins@citrine.io>
2020-09-04 13:09:39 -0700

    Fixed formatting

--- src/citrine/waiting/waiting.py
+++ src/citrine/waiting/waiting.py
@@ -8,35 +8,36 @@ from citrine.resources.workflow_executions import (
 )
 
 
-def wait_until(condition: Callable[[], bool], timeout: float = 900.0, interval: float = 3.0):
-    """Poll at the specified interval until the provided condition is truthy or the
-    timeout (in seconds) is reached."""
+def _wait_until(condition: Callable[[], bool], timeout: float = 900.0, interval: float = 3.0):
+    """Poll until the provided condition is truthy or the timeout is reached."""
     start = time()
     while not condition() and time() - start < timeout:
         sleep(interval)
     assert condition(), "Timeout reached, but condition is still False."
 
 
-def print_validation_status(
+def _print_validation_status(
     status: str, start_time: float, line_start: str = "", line_end: str = "\r"
 ):
     print(
-        f"{line_start}Status = {status:<25}Elapsed time = {str(int(time() - start_time)).rjust(3)}s",
+        f"{line_start}Status = {status:<25}Elapsed time"
+        " = {str(int(time() - start_time)).rjust(3)}s",
         end=line_end,
     )
 
 
-def pretty_execution_status(status: WorkflowExecutionStatus):
+def _pretty_execution_status(status: WorkflowExecutionStatus):
     status_text = status.status
     output_text = status_text if status_text != "InProgress" else "In progress"
     return output_text
 
 
-def print_execution_status(
+def _print_execution_status(
     status: WorkflowExecutionStatus, start_time: float, line_end: str = "\r"
 ):
     print(
-        f"Status = {pretty_execution_status(status):<25}Elapsed time = {str(int(time() - start_time)).rjust(3)}s",
+        f"Status = {_pretty_execution_status(status):<25}Elapsed time"
+        " = {str(int(time() - start_time)).rjust(3)}s",
         end=line_end,
     )
 
@@ -48,14 +49,15 @@ def wait_until_validated(
     timeout: float = 1800.0,
     interval: float = 3.0,
 ):
+    """Wait until module is validated."""
     start = time()
 
     def is_validated():
         status = collection.get(module.uid).status
-        print_validation_status(status, start)
+        _print_validation_status(status, start)
         return status != "VALIDATING"
 
-    wait_until(is_validated, timeout=timeout, interval=interval)
+    _wait_until(is_validated, timeout=timeout, interval=interval)
 
     if print_status_info:
         print("\nStatus info:")
@@ -64,11 +66,12 @@ def wait_until_validated(
 
 
 def wait_until_execution_is_finished(execution: WorkflowExecution):
+    """Wait until execution is finished."""
     start = time()
 
     def execution_is_finished():
         status = execution.status()
-        print_execution_status(status, start)
+        _print_execution_status(status, start)
         return not execution.status().in_progress
 
-    wait_until(execution_is_finished)
+    _wait_until(execution_is_finished)
