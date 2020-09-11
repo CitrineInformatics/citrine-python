52c80f778fe3a535500ca42a2a66b1ee74c61c2d
Max Hutchinson <maxhutch@gmail.com>
2020-09-11 10:49:54 -0400

    Add CrossValidationEvaluator

--- src/citrine/informatics/predictor_evaluator.py
+++ src/citrine/informatics/predictor_evaluator.py
@@ -1,11 +1,38 @@
-from typing import Set
+from typing import Set, Optional
 
+from citrine._serialization.polymorphic_serializable import PolymorphicSerializable
+from citrine._serialization.serializable import Serializable
 from citrine.informatics.predictor_evaluation_metrics import PredictorEvaluationMetric
 
 
-class PredictorEvaluator:
+class PredictorEvaluator(PolymorphicSerializable["PredictorEvaluator"]):
     def responses(self) -> Set[str]:
         raise NotImplementedError
 
     def metrics(self) -> Set[PredictorEvaluationMetric]:
         raise NotImplementedError
+
+
+class CrossValidationEvaluator(Serializable["CrossValidationEvaluator"], PredictorEvaluator):
+
+    def __init__(self, *,
+                 name: str,
+                 description: str = "",
+                 responses: Set[str],
+                 n_folds: int = 5,
+                 n_trials: int = 3,
+                 metrics: Optional[Set[PredictorEvaluationMetric]] = None,
+                 group_together: Optional[Set[str]] = None):
+        self.name = name
+        self.description = description
+        self._responses = responses
+        self._metrics = metrics
+        self.n_folds = n_folds
+        self.n_trials = n_trials
+        self.group_together = group_together or {}
+
+    def responses(self) -> Set[str]:
+        return self._responses
+
+    def metrics(self) -> Set[PredictorEvaluationMetric]:
+        return self._metrics
