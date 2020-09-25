53ac3eebcd7e55e6b9c387537f001cdd3020c7ca
Yoolhee Kim <yoolheekim@gmail.com>
2020-09-24 17:38:04 -0700

    adding more tests

--- tests/waiting/test_waiting.py
+++ tests/waiting/test_waiting.py
@@ -4,7 +4,7 @@ import mock
 import io
 import sys
 
-from citrine.waiting.waiting import wait_while_executing, wait_while_validating
+from citrine.waiting.waiting import wait_while_executing, wait_while_validating, ConditionTimeoutError
 
 def test_wait_while_validating():
 
@@ -22,7 +22,17 @@ def test_wait_while_validating():
     assert("VALID" in captured_output.getvalue())
 
 def test_wait_while_validating_timeout():
-    pass
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+
+    collection = mock.Mock()
+    module = mock.Mock()
+    statuses = mock.PropertyMock(side_effect = ["VALIDATING"] * 1000)
+    type(module).status = statuses
+    collection.get.return_value = module
+
+    with pytest.raises(ConditionTimeoutError):
+        wait_while_validating(collection, module, timeout=1.0)
 
 def test_wait_while_executing():
 
@@ -43,4 +53,17 @@ def test_wait_while_executing():
 
 
 def test_while_executing_timeout():
-    pass
+    
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+
+    workflow_execution = mock.Mock()
+    execution_status = mock.Mock()
+    statuses = mock.PropertyMock(side_effect = ["InProgress"]*1000)
+    in_progress = mock.PropertyMock(side_effect = [True]*1000)
+    type(execution_status).status = statuses
+    type(execution_status).in_progress = in_progress
+    workflow_execution.status.return_value = execution_status
+
+    with pytest.raises(ConditionTimeoutError):
+        wait_while_executing(workflow_execution)
