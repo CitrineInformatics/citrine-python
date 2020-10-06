87457f231dc62b0574b6a6504ee86b4d92d3a4ad
Max Hutchinson <maxhutch@gmail.com>
2020-10-05 22:58:50 -0400

    Remove debugging print that was left in #453
    Oops.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.72.0',
+      version='0.72.1',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Citrine Informatics',
