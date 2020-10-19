b83f4b0b215f9e61643a84023680709a45dddf44
Erin Antono <erin@citrine.io>
2020-10-19 14:50:25 -0700

    fixed waiting println and bumped version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.77.2',
+      version='0.77.3',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       author='Citrine Informatics',
