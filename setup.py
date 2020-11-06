bf1f40d38ce3c7e53226f971a06e3b520d74d559
Max Hutchinson <maxhutch@gmail.com>
2020-11-06 08:44:13 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.78.5',
+      version='0.78.6',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       long_description=long_description,
