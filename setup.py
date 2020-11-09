9ae603a337b9b0d099c4a0123f9c3e1f80461256
Max Hutchinson <maxhutch@gmail.com>
2020-11-09 08:18:53 -0500

    More documentation of ignore_when_grouping
    ignore_when_grouping is important, so give it a paragraph of docs
    and repeat it in the user guide and docstring.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.79.2',
+      version='0.79.3',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       long_description=long_description,
