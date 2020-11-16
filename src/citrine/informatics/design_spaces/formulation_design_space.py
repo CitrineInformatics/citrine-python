84c7e335d1a0854d5f5cf184a2c418adbccea548
Ryan Latture <rlatture@citrine.io>
2020-11-16 09:22:04 -0800

    Added schema_id to formulation design space.

--- src/citrine/informatics/design_spaces/formulation_design_space.py
+++ src/citrine/informatics/design_spaces/formulation_design_space.py
@@ -1,4 +1,5 @@
 from typing import Mapping, Set
+from uuid import UUID
 
 from citrine._rest.resource import Resource
 from citrine._serialization import properties
@@ -59,6 +60,8 @@ class FormulationDesignSpace(Resource['FormulationDesignSpace'], DesignSpace):
         serializable=False
     )
     module_type = properties.String('module_type', default='DESIGN_SPACE', deserializable=False)
+    schema_id = properties.UUID('schema_id', default=UUID('f3907a58-aa46-462c-8837-a5aa9605e79e'),
+                                deserializable=False)
 
     def __init__(self,
                  name: str,
