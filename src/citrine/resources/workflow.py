2579d96c5ee01730cb0ff5bd564c34920d07438e
Tatiana Kuzovleva <tkuzovleva@citrine.io>
2020-12-06 21:49:31 -0800

    PLA-4041 paginate design and the performance workflows

--- src/citrine/resources/workflow.py
+++ src/citrine/resources/workflow.py
@@ -1,6 +1,7 @@
 """Resources that represent collections of Workflows."""
+from itertools import chain
 from uuid import UUID
-from typing import TypeVar
+from typing import TypeVar, Optional, Callable, Tuple
 
 from citrine.informatics.workflows import Workflow
 from typing import Iterable
@@ -30,12 +31,48 @@ class WorkflowCollection(Collection[Workflow]):
         self.project_id = project_id
         self.session: Session = session
 
-    def _build_collection_elements(self,
-                                   collection: Iterable[dict]) -> Iterable[ResourceType]:
-        for element in collection:
-            if (element['module_type'] == 'DESIGN_WORKFLOW' or
-                    element['module_type'] == 'PERFORMANCE_WORKFLOW'):
-                yield self.build(element)
+    def list(self,
+             page: Optional[int] = None,
+             per_page: int = 100) -> Iterable[ResourceType]:
+        """
+        Paginate over the Design Workflows and Performance Workflows.
+
+        Leaving page and per_page as default values will yield all workflows,
+        paginating over all available pages.
+
+        Parameters
+        ---------
+        page: int, optional
+            The "page" of results to list. Default is to read all pages and yield
+            all results.  This option is deprecated.
+        per_page: int, optional
+            Max number of results to return per page. Default is 100.  This parameter
+            is used when making requests to the backend service.  If the page parameter
+            is specified it limits the maximum number of elements in the response.
+
+        Returns
+        -------
+        Iterable[ResourceType]
+            Resources in this collection.
+
+        """
+        # List all Design Workflows
+        design_workflows = self._paginator.paginate(
+            page_fetcher=self._fetch_design_page,
+            collection_builder=self._build_collection_elements,
+            page=page,
+            per_page=per_page
+        )
+
+        # List all Performance Workflows
+        performance_workflows = self._paginator.paginate(
+            page_fetcher=self._fetch_performance_page,
+            collection_builder=self._build_collection_elements,
+            page=page,
+            per_page=per_page
+        )
+
+        return chain(design_workflows, performance_workflows)
 
     def build(self, data: dict) -> Workflow:
         """Build an individual Workflow."""
@@ -43,3 +80,33 @@ class WorkflowCollection(Collection[Workflow]):
         workflow.session = self.session
         workflow.project_id = self.project_id
         return workflow
+
+    def _fetch_design_page(self,
+                           path: Optional[str] = None,
+                           fetch_func: Optional[Callable[..., dict]] = None,
+                           page: Optional[int] = None,
+                           per_page: Optional[int] = None,
+                           json_body: Optional[dict] = None,
+                           additional_params: Optional[dict] = None
+                           ) -> Tuple[Iterable[dict], str]:
+
+        if additional_params is None:
+            additional_params = {"module_type": "DESIGN_WORKFLOW"}
+        else:
+            additional_params.update({"module_type": "DESIGN_WORKFLOW"})
+        return self._fetch_page(path, fetch_func, page, per_page, json_body, additional_params)
+
+    def _fetch_performance_page(self,
+                                path: Optional[str] = None,
+                                fetch_func: Optional[Callable[..., dict]] = None,
+                                page: Optional[int] = None,
+                                per_page: Optional[int] = None,
+                                json_body: Optional[dict] = None,
+                                additional_params: Optional[dict] = None,
+                                ) -> Tuple[Iterable[dict], str]:
+
+        if additional_params is None:
+            additional_params = {"module_type": "PERFORMANCE_WORKFLOW"}
+        else:
+            additional_params.update({"module_type": "PERFORMANCE_WORKFLOW"})
+        return self._fetch_page(path, fetch_func, page, per_page, json_body, additional_params)
