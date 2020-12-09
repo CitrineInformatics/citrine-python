a39fd810940c20fc96811ae115cd9d16520db695
Brendan Folie <bfolie@citrine.io>
2020-12-08 16:01:44 -0800

    Patch version bump

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.85.0',
+      version='0.85.1',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       long_description=long_description,
