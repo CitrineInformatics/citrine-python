da3f27a82ce639b6d301917da591594b5bc4d0e8
Ryan Latture <rlatture@citrine.io>
2020-12-15 12:43:20 -0800

    Fixed flake8 violations.

--- src/citrine/__init__.py
+++ src/citrine/__init__.py
@@ -1,6 +1,6 @@
 # TODO: Add a docstring here
 from citrine.citrine import Citrine  # noqa: F401
 import logging
-from .__version__ import __version__
+from .__version__ import __version__  # noqa: F401
 
 logging.basicConfig(level=logging.WARNING)
--- src/citrine/__version__.py
+++ src/citrine/__version__.py
@@ -1,2 +1,1 @@
-# flake8: noqa
 __version__ = '0.85.3'
