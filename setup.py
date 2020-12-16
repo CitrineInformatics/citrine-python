dd707dccc80d90500496d268cad8e0c90198fb9c
Ryan Latture <rlatture@citrine.io>
2020-12-16 08:33:55 -0800

    Version bump.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class PreBuildCommand(build_py):
 
 
 setup(name='citrine',
-      version='0.85.3',
+      version='0.85.4',
       url='http://github.com/CitrineInformatics/citrine-python',
       description='Python library for the Citrine Platform',
       long_description=long_description,
