a977fc09d39663f37e97075e5fbba48f8b1eaa31
Ventura Rivera <vrivera@citrine.io>
2021-03-09 14:25:52 -0800

    adding dataset_deletes module and clear_dataset function

---
+++ src/citrine/seeding/dataset_deletes.py
@@ -0,0 +1,53 @@
+"""Functions that perform dataset deletes."""
+from typing import List, Tuple
+
+from citrine.resources.api_error import ApiError
+from citrine.resources.dataset import Dataset
+
+from gemd.entity.link_by_uid import LinkByUID
+
+
+def clear_dataset(dataset: Dataset, delete_templates: bool = False) -> \
+        List[Tuple[LinkByUID, ApiError]]:
+    """
+    Clears the GEMD objects out of a dataset.
+
+    Parameters
+    ----------
+    dataset : Dataset
+        The dataset to remove GEMD objects from
+    include_templates : bool, optional
+        An option to also remove templates from the given dataset, by default False
+
+    Returns
+    -------
+    List[Tuple[LinkByUID, ApiError]]
+        A list of (LinkByUID, api_error) for each failure to delete an object.
+        Note that this method doesn't raise an exception if an object fails to be
+        deleted.
+
+    """
+    print("Collecting GEMDs...")
+    gemds = [
+        *dataset.ingredient_runs.list_all(),
+        *dataset.ingredient_specs.list_all(),
+        *dataset.material_runs.list_all(),
+        *dataset.material_specs.list_all(),
+        *dataset.measurement_runs.list_all(),
+        *dataset.measurement_specs.list_all(),
+        *dataset.process_runs.list_all(),
+        *dataset.process_specs.list_all(),
+    ]
+    if delete_templates:
+        gemds += [
+            *dataset.material_templates.list_all(),
+            *dataset.measurement_templates.list_all(),
+            *dataset.process_templates.list_all(),
+            # uncomment these when attribute template delete is possible
+            # *dataset.property_templates.list_all(),
+            # *dataset.condition_templates.list_all(),
+            # *dataset.parameter_templates.list_all(),
+        ]
+    print("Deleting GEMDs...")
+    del_response = dataset.gemd_batch_delete(gemds)
+    return del_response
