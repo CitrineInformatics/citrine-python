6515d0427ac8a84eb82e69507e2c517d7e744a3d
Ventura Rivera <35743526+ventura-rivera@users.noreply.github.com>
2021-04-13 09:41:25 -0700

    uncommenting attribute template delete in tests

--- tests/seeding/test_dataset_deletes.py
+++ tests/seeding/test_dataset_deletes.py
@@ -236,10 +236,9 @@ def test_wipe_dataset(fake_dataset):
         *fake_dataset.material_templates.list_all(),
         *fake_dataset.measurement_templates.list_all(),
         *fake_dataset.process_templates.list_all(),
-        # TODO: uncomment these when attribute template delete is possible
-        # *fake_dataset.property_templates.list_all(),
-        # *fake_dataset.condition_templates.list_all(),
-        # *fake_dataset.parameter_templates.list_all(),
+        *fake_dataset.property_templates.list_all(),
+        *fake_dataset.condition_templates.list_all(),
+        *fake_dataset.parameter_templates.list_all(),
     ])
     
     # Clear GEMD objects only (not templates)
