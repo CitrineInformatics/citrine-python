732986b539b786bd54adb01483491600cdabc7e9
Brendan Folie <bfolie@citrine.io>
2021-05-24 17:55:19 -0700

    remove unused import

--- src/citrine/resources/material_run.py
+++ src/citrine/resources/material_run.py
@@ -9,7 +9,7 @@ from citrine._rest.resource import Resource
 from citrine._serialization.properties import List as PropertyList
 from citrine._serialization.properties import Optional as PropertyOptional
 from citrine._serialization.properties import String, LinkOrElse, Mapping, Object
-from citrine.resources.data_concepts import DataConcepts, CITRINE_SCOPE, _make_link_by_uid
+from citrine.resources.data_concepts import DataConcepts, _make_link_by_uid
 from citrine.resources.material_spec import MaterialSpecCollection
 from citrine.resources.object_runs import ObjectRun, ObjectRunCollection
 from gemd.entity.file_link import FileLink
