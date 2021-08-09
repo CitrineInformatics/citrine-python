ec51ab45c70c0ef91cff7aba842b8162d338b1c2
Daphne Kao <dkao@citrine.io>
2021-08-09 10:34:56 -0500

    PLA-7736: Clean up formatting and unnecessary code in test_waiting.py

--- tests/jobs/test_waiting.py
+++ tests/jobs/test_waiting.py
@@ -8,8 +8,12 @@ import time
 
 from citrine.informatics.executions.design_execution import DesignExecution
 from citrine.informatics.executions.predictor_evaluation_execution import PredictorEvaluationExecution
-from src.citrine.jobs.waiting import wait_for_asynchronous_object, wait_while_executing, wait_while_validating, ConditionTimeoutError
-
+from citrine.jobs.waiting import (
+    wait_for_asynchronous_object,
+    wait_while_executing,
+    wait_while_validating,
+    ConditionTimeoutError
+)
 
 @mock.patch('time.sleep', return_value=None)
 def test_wait_while_validating(sleep_mock):
@@ -31,7 +35,6 @@ def test_wait_while_validating(sleep_mock):
     assert("Status = VALID" in captured_output.getvalue())
     assert("The predictor is now validated." in captured_output.getvalue())
 
-
 @mock.patch('time.time')
 @mock.patch('time.sleep', return_value=None)
 def test_wait_while_validating_timeout(sleep_mock, time_mock):
@@ -39,9 +42,6 @@ def test_wait_while_validating_timeout(sleep_mock, time_mock):
         time.mktime(datetime(2020, 10, 30).timetuple()),
         time.mktime(datetime(2020, 10, 31).timetuple())
     ]
-
-    captured_output = io.StringIO()
-    sys.stdout = captured_output
 
     collection = mock.Mock()
     module = mock.Mock()
@@ -52,7 +52,6 @@ def test_wait_while_validating_timeout(sleep_mock, time_mock):
 
     with pytest.raises(ConditionTimeoutError) as exceptio:
         wait_while_validating(collection=collection, module=module, timeout=1.0)
-
 
 @mock.patch('time.sleep', return_value=None)
 def test_wait_while_executing(sleep_mock):
