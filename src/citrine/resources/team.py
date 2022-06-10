aa6891091028832cb950d49de0d4a71f897e4074
Chris LeBlanc <crleblanc@gmail.com>
2022-06-10 15:11:10 -0700

    Flake8

--- src/citrine/resources/team.py
+++ src/citrine/resources/team.py
@@ -247,8 +247,8 @@ class Team(Resource['Team']):
         """
         query_params = {"domain": self._path(), "action": action}
         return self.session.checked_get(f"/{resource_type}/authorized-ids",
-            params=query_params,
-            version="v3")['ids']
+                                        params=query_params,
+                                        version="v3")['ids']
 
     @property
     def projects(self) -> ProjectCollection:
