4a8c1a81245083097b966d8aeefc804ba41d4357
Kevin Williams <kwilliams@citrine.io>
2022-12-07 14:34:58 -0500

    More flake8

--- src/citrine/resources/data_version_update.py
+++ src/citrine/resources/data_version_update.py
@@ -7,7 +7,7 @@ from citrine._serialization.serializable import Serializable
 
 
 class DataVersionUpdate(Serializable['DataVersionUpdate']):
-    """ Container for data updates. """
+    """Container for data updates."""
 
     current = _properties.String('current')
     latest = _properties.String('latest')
@@ -22,10 +22,7 @@ class DataVersionUpdate(Serializable['DataVersionUpdate']):
 
 
 class BranchDataUpdate(Resource['BranchDataUpdate']):
-    """
-    Branch data updates with predictors using the versions indicated that
-    are in READY status.
-    """
+    """Branch data updates with predictors using the versions indicated."""
 
     data_updates = _properties.List(_properties.Object(DataVersionUpdate), "data_updates")
     predictors = _properties.List(_properties.Object(PredictorRef), "predictors")
@@ -39,8 +36,7 @@ class BranchDataUpdate(Resource['BranchDataUpdate']):
 
 class NextBranchVersionRequest(Resource['NextBranchVersionRequest']):
     """
-    Instructions for how the next version of a branch should handle its predictors when the
-    workflows are cloned.
+    Instructions for how the next version of a branch should handle its predictors.
 
     data_updates contains the list of data source versions to upgrade
     (current->latest), and use_predictors will wither have a <predictor_id>:latest to indicate
