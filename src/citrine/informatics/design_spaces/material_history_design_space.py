1afa1e8e6971e739c5c9ba32e1b16346ef513e3f
Sean Friedowitz <sfriedowitz@citrine.io>
2023-02-28 14:19:58 -0800

    got serde kinda working

--- src/citrine/informatics/design_spaces/material_history_design_space.py
+++ src/citrine/informatics/design_spaces/material_history_design_space.py
@@ -1,8 +1,9 @@
-from typing import List, Optional
-from copy import deepcopy
+from typing import Optional, Sequence, Set
+from uuid import UUID
 
 from citrine._rest.resource import Resource, ResourceTypeEnum
 from citrine._serialization import properties
+from citrine._serialization.serializable import Serializable
 from citrine.informatics.design_spaces import FormulationDesignSpace
 from citrine.informatics.design_spaces.design_space import DesignSpace
 from citrine._rest.ai_resource_metadata import AIResourceMetadata
@@ -10,80 +11,122 @@ from citrine._rest.ai_resource_metadata import AIResourceMetadata
 from citrine.informatics.dimensions import Dimension
 
 __all__ = [
-    'MaterialNodeDefinition',
-    'MaterialHistoryDesignSpace'
+    "TemplateLink",
+    "MaterialNodeDefinition",
+    "MaterialHistoryDesignSpace"
 ]
+    
 
-
-class CustomIdentifier(Resource['CustomIdentifier']):
-
-    identifier = properties.String("id")
-    scope = properties.Optional(properties.String, "scope")
-
-    def __init__(self):
-        pass  # pragma: no cover
-
-
-class TemplateIdentifier(Resource['TemplateIdentifier']):
-    """A node identifier that links to a GEMD material and process template."""
-
-    template_name = properties.String("templateName")
-    material_template = properties.UUID("materialTemplate")
-    process_template = properties.UUID("processTemplate")
-
-    def __init__(self):
-        pass  # pragma: no cover
-
-
-class MaterialNodeDefinition(Resource['MaterialNodeDefinition']):
-    """A single node in a MaterialHistoryDesignSpace."""
-
-    identifier = properties.Object(CustomIdentifier, "identifier")
-    attributes = properties.Set(properties.Object(Dimension), "attributes")
-    formulation = properties.Optional(properties.Object(FormulationDesignSpace), "formulation")
-    template = properties.Optional(properties.Object(TemplateIdentifier), "template")
-
-    def __init__(self):
-        pass  # pragma: no cover
-
-    @classmethod
-    def _pre_build(cls, data: dict) -> dict:
-        print(data)
-        if "formulation" in data:
-            data["formulation"] = {"config": data["formulation"]}
-        return data
-
-
-class MaterialHistoryDesignSpace(Resource['ProductDesignSpace'], DesignSpace, AIResourceMetadata):
-    """A design space that can produce candidates as material histories.
+class TemplateLink(Serializable["TemplateLink"]):
+    """Link to a set of material/process templates for a material node definition.
 
     Parameters
     ----------
-    name:str
+    material_template: UUID
+        Citrine ID referencing an on-platform material template.
+    process_template: UUID
+        Citrine ID referencing an on-platform process template.
+    name: str
+        A custom name used to refer to the combination of material/process templates.
+        (Default: "")
+    """
+
+    name = properties.String("name")
+    material_template = properties.UUID("material_template")
+    process_template = properties.UUID("process_template")
+
+    def __init__(
+            self,
+            *,
+            material_template: UUID,
+            process_template: UUID,
+            name: str = "",
+    ):
+        self.name = name
+        self.material_template = material_template
+        self.process_template = process_template
+
+
+class MaterialNodeDefinition(Serializable["MaterialNodeDefinition"]):
+    """A single node in a material history design space.
+
+    Parameters
+    ----------
+    name: str
         the name of the design space
-    description:str
+    scope: str
+
+    """
+
+    name = properties.String("identifier.id")
+    scope = properties.Optional(properties.String, "identifier.scope")
+    attributes = properties.Set(properties.Object(Dimension), "attributes")
+    formulation_subspace = properties.Optional(
+        properties.Object(FormulationDesignSpace), "formulation"
+    )
+    template_link = properties.Optional(properties.Object(TemplateLink), "template")
+
+    def __init__(
+            self,
+            *,
+            name: str,
+            scope: Optional[str] = None,
+            attributes: Optional[Sequence[Dimension]] = None,
+            formulation_subspace: Optional[FormulationDesignSpace] = None,
+            template_link: Optional[TemplateLink] = None
+    ):
+        self.name = name
+        self.scope = scope
+        self.attributes = attributes or set()
+        self.formulation_subspace = formulation_subspace
+        self.template_link = template_link
+
+    @property
+    def template_name(self) -> Optional[str]:
+        return self.template_link.name if self.template_link else None
+
+    @property
+    def material_template(self) -> Optional[UUID]:
+        return self.template_link.material_template if self.template_link else None
+
+    @property
+    def process_template(self) -> Optional[UUID]:
+        return self.template_link.process_template if self.template_link else None
+
+    def __repr__(self):
+        return "<MaterialNodeDefinition {!r}>".format(self.name)
+
+
+class MaterialHistoryDesignSpace(
+    Resource["MaterialHistoryDesignSpace"], DesignSpace, AIResourceMetadata
+):
+    """A design space that produces candidates as material histories.
+
+    Parameters
+    ----------
+    name: str
+        the name of the design space
+    description: str
         the description of the design space
     root: MaterialNodeDefinition
-        the root node of the material history candidates
-    subspaces: List[MaterialNodeDefinition]
-        the list of subspaces to combine, containing dimensions and optional formulation spaces
+        the terminal material node produced by the design space
+    subspaces: Set[MaterialNodeDefinition]
+        the sub material nodes produced by the design space
 
     """
 
     _resource_type = ResourceTypeEnum.MODULE
 
-    root = properties.Object(MaterialNodeDefinition, 'config.root')
-    subspaces = properties.List(
-        properties.Object(MaterialNodeDefinition), 'config.subspaces', default=[]
+    root = properties.Object(MaterialNodeDefinition, "config.root")
+    subspaces = properties.Set(
+        properties.Object(MaterialNodeDefinition), "config.subspaces", default=set()
     )
 
-    # Product design spaces should not be embedded in other subspaces, hence status is required
-    status = properties.String('status', serializable=False)
-
+    status = properties.String("status", serializable=False)
+    module_type = properties.String("module_type", default="DESIGN_SPACE")
     typ = properties.String(
-        'config.type', default='MaterialHistoryDesignSpace', deserializable=False
+        "config.type", default="MaterialHistoryDesignSpace", deserializable=False
     )
-    module_type = properties.String('module_type', default='DESIGN_SPACE')
 
     def __init__(
             self,
@@ -91,38 +134,42 @@ class MaterialHistoryDesignSpace(Resource['ProductDesignSpace'], DesignSpace, AI
             *,
             description: str,
             root: MaterialNodeDefinition,
-            subspaces: Optional[List[MaterialNodeDefinition]] = None
+            subspaces: Optional[Set[MaterialNodeDefinition]] = None
     ):
         self.name: str = name
         self.description: str = description
         self.root: MaterialNodeDefinition = root
-        self.subspaces: List[MaterialNodeDefinition] = subspaces or []
-
-    def dump(self) -> dict:
-        """Override dump to replace on-platform subspaces with their uids."""
-        model_copy = deepcopy(self)
-        for i, subspace in enumerate(model_copy.subspaces):
-            if isinstance(subspace, DesignSpace) and subspace.uid is not None:
-                model_copy.subspaces[i] = subspace.uid
-        serialized = properties.Object(ProductDesignSpace).serialize(model_copy)
-        return self._post_dump(serialized)
-
-    def _post_dump(self, data: dict) -> dict:
-        data['display_name'] = data['config']['name']
-        for i, subspace in enumerate(data['config']['subspaces']):
-            if isinstance(subspace, dict):
-                # embedded design spaces are not modules, so only serialize their config
-                data['config']['subspaces'][i] = subspace['config']
-        return data
+        self.subspaces: Set[MaterialNodeDefinition] = subspaces or set()
 
     @classmethod
     def _pre_build(cls, data: dict) -> dict:
-        subspaces = data['config'].get('subspaces', [])
-        # For each subspace, rename the `instance` key to `config`.
-        for i, _ in enumerate(subspaces):
-            data['config']['subspaces'][i]['config'] = \
-                data['config']['subspaces'][i].pop('instance')
+        data["config"]["root"] = cls.__build_format_node(data["config"]["root"])
+        data["config"]["subspaces"] = {
+            cls.__build_format_node(node) for node in data["config"]["subspaces"]
+        }
         return data
 
-    def __str__(self):
-        return '<MaterialHistoryDesignSpace {!r}>'.format(self.name)
+    @staticmethod
+    def __build_format_node(node: dict) -> dict:
+        # Wrap formulation in a "config" block so it can be deserialized as FormulationDesignSpace
+        formulation_data = node.pop("formulation")
+        if formulation_data:
+            node["formulation"] = {"config": formulation_data}
+        return node
+
+    def _post_dump(self, data: dict) -> dict:
+        data["config"]["root"] = self.__dump_format_node(data["config"]["root"])
+        data["config"]["subspaces"] = {
+            self.__dump_format_node(x) for x in data["config"]["subspaces"]
+        }
+        return data
+
+    @staticmethod
+    def __dump_format_node(node: dict) -> dict:
+        formulation = node.pop("formulation")
+        if formulation:
+            node["formulation"] = formulation["config"]
+        return node
+
+    def __repr__(self):
+        return "<MaterialHistoryDesignSpace {!r}>".format(self.name)
