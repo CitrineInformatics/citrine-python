81dab020c4c8aa7ddaf6115b432c842fac692a36
Sean Friedowitz <sfriedowitz@citrine.io>
2023-02-28 17:59:16 -0800

    add helper method and docs

--- src/citrine/informatics/design_spaces/material_history_design_space.py
+++ src/citrine/informatics/design_spaces/material_history_design_space.py
@@ -1,50 +1,62 @@
-from typing import Optional, Sequence, Set
+from typing import Optional, Set, Union
 from uuid import UUID
 
+from gemd import LinkByUID
+
+from citrine._rest.ai_resource_metadata import AIResourceMetadata
 from citrine._rest.resource import Resource, ResourceTypeEnum
 from citrine._serialization import properties
 from citrine._serialization.serializable import Serializable
+from citrine.resources.project import Project
+from citrine.resources.material_template import MaterialTemplate
+from citrine.resources.process_template import ProcessTemplate
+from citrine.informatics.dimensions import Dimension
 from citrine.informatics.design_spaces import FormulationDesignSpace
 from citrine.informatics.design_spaces.design_space import DesignSpace
-from citrine._rest.ai_resource_metadata import AIResourceMetadata
-
-from citrine.informatics.dimensions import Dimension
 
 __all__ = [
     "TemplateLink",
     "MaterialNodeDefinition",
     "MaterialHistoryDesignSpace"
 ]
-    
+
 
 class TemplateLink(Serializable["TemplateLink"]):
-    """Link to a set of material/process templates for a material node definition.
+    """Link to a Citrine Platform material and process template for a material node definition.
 
     Parameters
     ----------
-    material_template: UUID
+    project: Project
+        Project containing the material and process templates,
+        used to resolve Citrine Platform UUIDs.
+    material_template: Union[UUID, str, LinkByUid, MaterialTemplate]
         Citrine ID referencing an on-platform material template.
-    process_template: UUID
+    process_template: Union[UUID, str, LinkByUid, ProcessTemplate]
         Citrine ID referencing an on-platform process template.
-    name: str
-        A custom name used to refer to the combination of material/process templates.
-        (Default: "")
     """
 
-    name = properties.String("name")
     material_template = properties.UUID("material_template")
     process_template = properties.UUID("process_template")
 
+    _name = properties.String("name")
+    """Simple name used to refer to the combination of material and process template."""
+
+    MaterialTemplateType = Union[UUID, str, LinkByUID, MaterialTemplate]
+    ProcessTemplateType = Union[UUID, str, LinkByUID, ProcessTemplate]
+
     def __init__(
             self,
+            project: Project,
             *,
-            material_template: UUID,
-            process_template: UUID,
-            name: str = "",
+            material_template: MaterialTemplateType,
+            process_template: ProcessTemplateType
     ):
-        self.name = name
-        self.material_template = material_template
-        self.process_template = process_template
+        resolved_mat = project.gemd.get(material_template)
+        resolved_proc = project.gemd.get(process_template)
+
+        self.material_template = resolved_mat.uids["id"]
+        self.process_template = resolved_proc.uids["id"]
+        self._name = f"{resolved_proc.name}-{resolved_mat.name}"
 
 
 class MaterialNodeDefinition(Serializable["MaterialNodeDefinition"]):
@@ -54,18 +66,18 @@ class MaterialNodeDefinition(Serializable["MaterialNodeDefinition"]):
     ----------
     name: str
         A unique name used to reference the materials produced by this node in the design space.
-        If the material produced by this node is used as an ingredient in other node definitions,
-        this name should appear as an ingredient name in the other node's formulation subspace.
+        This name should be used to identify the material when used as an ingredient
+        in the formulation subspace of a different node.
     scope: Optional[str]
-        An optional custom scope used to identify materials produced by this design space.
+        An optional custom scope used to identify the materials produced by this node.
     attributes: Set[Dimension]
         Set of dimensions included on materials produced by this node.
     formulation_subspace: Optional[FormulationDesignSpace]
         An optional formulation design space defining the ingredients, labels,
-        and constraints for formulations produced by this node.
+        and constraints for formulations in materials produced by this node.
     template_link: Optional[TemplateLink]
-        A set of identifiers linking materials produced by this node to Citrine Platform
-        material and process templates.
+        A set of identifiers linking the materials produced by this node to
+        material and process templates on the Citrine Platform.
 
     """
 
@@ -113,14 +125,23 @@ class MaterialHistoryDesignSpace(
 ):
     """A design space that produces material history candidates.
 
-    A material history design space begins with a root node that defines the
-    attributes and formulations included on the terminal materials of the resulting histories.
-    It also includes a set of sub-nodes that can be used to define the structure of any new
-    intermediate mixtures that appear in the history of the root node.
+    A material history design space always contains a root node that defines the
+    attributes and formulation contents included on terminal materials of the candidates.
+    It also includes a set of sub-nodes that can be used to define the any new
+    intermediate mixtures that appear in the history of the terminal material.
 
-    The process structure of the material history candidates is defined through the `name`
-    and `formulation_subspace` fields of the material node definitions.
+    Material histories produced by this design space are connected based on the
+    name identifiers and formulation contents of each node.
+    For example, the root node may contain a formulation subspace with an ingredient named
+    'New Mixture-001'. If this ingredient matches the `name` field on one of the sub-nodes
+    in the design space, the resulting candidates will contain a terminal material with this
+    new mixture as one of its ingredients. This procedure can be extended to sub-nodes
+    referencing other sub-nodes, allowing for the linkage of complex material history shapes
+    in the resulting candidates.
 
+    Every node in the design space also contains a set of `Dimension`s used to define any
+    attributes (i.e., properties, processing parameters) that should appear on new materials
+    produced by that node.
 
     Parameters
     ----------
