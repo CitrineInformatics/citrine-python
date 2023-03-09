c64af7078a6b0dded606b150844c713a0bee8072
Jamie Heller <jheller@citrine.io>
2023-03-09 11:29:26 -0500

    PLA-11242 Implement predict method on predictor

---
+++ src/citrine/informatics/predictors/single_predict_request.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+from uuid import UUID
+
+from citrine._serialization import properties
+from citrine._serialization.serializable import Serializable
+from citrine.informatics.design_candidate import DesignMaterial
+
+
+class SinglePredictRequest(Serializable["SinglePredictRequest"]):
+    """A Citrine Single Predict Request.
+
+    This class represents a request to make a prediction against a predictor.
+    """
+
+    material_id = properties.UUID('material_id')
+    identifiers = properties.List(properties.String(), 'identifiers')
+    material = properties.Object(DesignMaterial, 'material')
+    random_seed = properties.Optional(properties.Integer, 'random_seed')
+
+    def __init__(self, material_id: UUID,
+                 identifiers: List[str],
+                 material: DesignMaterial,
+                 *,
+                 random_seed: Optional[int] = None):
+        self.material_id = material_id
+        self.identifiers = identifiers
+        self.material = material
+        self.random_seed = random_seed
---
+++ src/citrine/informatics/predictors/single_prediction.py
@@ -0,0 +1,24 @@
+from typing import List
+from uuid import UUID
+
+from citrine._serialization import properties
+from citrine._serialization.serializable import Serializable
+from citrine.informatics.design_candidate import DesignMaterial
+
+
+class SinglePrediction(Serializable["SinglePrediction"]):
+    """A Citrine Single Prediction.
+
+    This class represents the result of a prediction made using a predictor.
+    """
+
+    material_id = properties.UUID('material_id')
+    identifiers = properties.List(properties.String(), 'identifiers')
+    material = properties.Object(DesignMaterial, 'material')
+
+    def __init__(self, material_id: UUID,
+                 identifiers: List[str],
+                 material: DesignMaterial):
+        self.material_id = material_id
+        self.identifiers = identifiers
+        self.material = material
