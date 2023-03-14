00ae5924c59e7bef439d5705054f655dfccc4371
Sean Friedowitz <sfriedowitz@citrine.io>
2023-03-14 09:56:35 -0700

    delete convenience script

--- scripts/run_pytest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-# Helper script to execute pytest with coverage when invoked from the citrine-python repository directory
-# This is invoked by the Travis test job, so is useful to check for when developing locally
-
-pytest --cov=src/ --cov-report term-missing:skip-covered --cov-config=tox.ini --cov-fail-under=100 -r .
