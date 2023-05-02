0f922bce63983a17c2ab6edf41972413120bbd94
Ken Kroenlein <kkroenlein@citrine.io>
2023-05-02 11:50:35 -0600

    Add 3.11 to testing suite

--- setup.py
+++ setup.py
@@ -50,5 +50,6 @@ setup(name='citrine',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
       ],
-)
+      )
