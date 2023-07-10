1f4521f62b2c315a4bbd9a3811a86cee4c2e322f
Sean Friedowitz <sfriedowitz@citrine.io>
2023-07-09 17:46:04 -0700

    fix some test coverage

--- tests/informatics/test_informatics.py
+++ tests/informatics/test_informatics.py
@@ -3,7 +3,7 @@ import pytest
 from citrine.informatics.descriptors import FormulationDescriptor, FormulationKey
 from citrine.informatics.constraints import ScalarRangeConstraint, AcceptableCategoriesConstraint, \
     IngredientCountConstraint, IngredientFractionConstraint, IngredientRatioConstraint, \
-    LabelFractionConstraint
+    LabelFractionConstraint, IntegerRangeConstraint
 from citrine.informatics.design_spaces import ProductDesignSpace, EnumeratedDesignSpace, FormulationDesignSpace
 from citrine.informatics.objectives import ScalarMaxObjective, ScalarMinObjective
 from citrine.informatics.scores import LIScore, EIScore, EVScore
@@ -26,6 +26,7 @@ informatics_string_data = [
         max=1
     ), f"<LabelFractionConstraint '{FormulationKey.HIERARCHICAL.value}'::'y'>"),
     (ScalarRangeConstraint(descriptor_key='z'), "<ScalarRangeConstraint 'z'>"),
+    (IntegerRangeConstraint(descriptor_key='w'), "<IntegerRangeConstraint 'w'>"),
     (AcceptableCategoriesConstraint(descriptor_key='x', acceptable_categories=[]), "<AcceptableCategoriesConstraint 'x'>"),
     (IngredientRatioConstraint(formulation_descriptor=FormulationDescriptor('Flat Formulation'), min=0.0, max=1.0, ingredient=("x", 1.5), label=("x'", 0.5), basis_ingredients=["y", "z"], basis_labels=["y'", "z'"]), "<IngredientRatioConstraint 'Flat Formulation'>"),
     (ProductDesignSpace(name='my design space', description='does some things'),
