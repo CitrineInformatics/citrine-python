c11148868179d4088e264e66b43510222bbb30e0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2023-09-20 19:31:22 +0000

    Deploy CitrineInformatics/citrine-python to github.com/CitrineInformatics/citrine-python.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.34.0',
+    VERSION: '2.36.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
