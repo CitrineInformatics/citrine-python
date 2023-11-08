97d612d2d32cf10e7d5cc06d5c51d51b313fe5ad
Kaai Kauwe <108346431+StevenKauwe@users.noreply.github.com>
2023-11-08 16:01:11 -0500

    add helper for internal user check

--- src/citrine/resources/catalyst.py
+++ src/citrine/resources/catalyst.py
@@ -1,5 +1,5 @@
-from citrine.informatics.catalyst.assistant import AssistantResponse, AssistantRequest
 from citrine.informatics.catalyst.insights import InsightsResponse, InsightsRequest
+from citrine.informatics.catalyst.assistant import AssistantResponse, AssistantRequest
 from citrine.informatics.predictors.graph_predictor import GraphPredictor
 from citrine.resources.user import UserCollection
 from citrine._session import Session
@@ -9,8 +9,8 @@ from citrine._utils.functions import resource_path
 class CatalystResource:
     """Encapsulates th ability to invoke Catalyst."""
 
-    _path_template: str = "/catalyst"
-    _api_version = "v1"
+    _path_template: str = '/catalyst'
+    _api_version = 'v1'
 
     def __init__(self, session: Session):
         self.session: Session = session
@@ -18,6 +18,16 @@ class CatalystResource:
     def _get_path(self, action: str):
         """Construct a Catalyst url from a base path and action."""
         return resource_path(path_template=self._path_template, action=action)
+
+    def _ensure_internal_user(self):
+        """Ensure that the user is internal; raise an error otherwise."""
+        user = UserCollection(self.session).me()
+        if not user.is_internal:
+            raise NotImplementedError(
+                "I'm sorry, but this feature is not currently available for "
+                "your organization. Please watch the release notes for "
+                "updates."
+            )
 
     def assistant(self, query: str, *, predictor: GraphPredictor) -> AssistantResponse:
         """Invoke the model assistant.
@@ -34,13 +44,7 @@ class CatalystResource:
         The assistant response, containing details of the result of your query which vary by type.
 
         """
-        user = UserCollection(self.session).me()
-        if not user.is_internal:
-            raise NotImplementedError(
-                "I'm sorry, but this feature is not currently available for "
-                "your organization. Please watch the release notes for "
-                "updates."
-            )
+        self._ensure_internal_user()
 
         payload = AssistantRequest(question=query, predictor=predictor).dump()
         path = self._get_path("assistant")
@@ -60,13 +64,7 @@ class CatalystResource:
         The insights response, containing details of the result of your query which vary by type.
 
         """
-        user = UserCollection(self.session).me()
-        if not user.is_internal:
-            raise NotImplementedError(
-                "I'm sorry, but this feature is not currently available for "
-                "your organization. Please watch the release notes for "
-                "updates."
-            )
+        self._ensure_internal_user()
 
         payload = InsightsRequest(question=query).dump()
         path = self._get_path(["documents", "search"])
