ab2ac9b543693992659dd663f66f3e005346c5e7
Austin Noto-Moniz <anoto-moniz@citrine.io>
2023-12-15 15:38:21 -0500

    [PLA-12982] DW use branch root ID and version number.
    The API still expects design workflows to have a branch version ID, and
    will return design workflows with one. As such, this updates the
    DesignWorkflowCollection to look up the version ID before making a
    request, and the root ID and version befor returning the result.

--- src/citrine/resources/design_workflow.py
+++ src/citrine/resources/design_workflow.py
@@ -4,6 +4,7 @@ from uuid import UUID
 
 from citrine._rest.collection import Collection
 from citrine._session import Session
+from citrine.exceptions import NotFound
 from citrine.informatics.workflows import DesignWorkflow
 from citrine.resources.response import Response
 from functools import partial
@@ -18,10 +19,34 @@ class DesignWorkflowCollection(Collection[DesignWorkflow]):
     _resource = DesignWorkflow
     _api_version = "v2"
 
-    def __init__(self, project_id: UUID, session: Session, branch_id: UUID = None):
+    def __init__(self,
+                 project_id: UUID,
+                 session: Session,
+                 branch_root_id: Optional[UUID] = None,
+                 branch_version: Optional[int] = None):
         self.project_id: UUID = project_id
         self.session: Session = session
-        self.branch_id: UUID = branch_id
+        self.branch_root_id: UUID = branch_root_id
+        self.branch_version: int = branch_version
+
+    def _resolve_branch_root_and_version(self, workflow):
+        from citrine.resources.branch import BranchCollection
+
+        workflow_copy = deepcopy(workflow)
+        bc = BranchCollection(self.project_id, self.session)
+        branch = bc.get_by_version_id(version_id=workflow_copy._branch_id)
+        workflow_copy._branch_root_id = branch.root_id
+        workflow_copy._branch_version = branch.version
+        return workflow_copy
+
+    def _resolve_branch_id(self, root_id, version):
+        from citrine.resources.branch import BranchCollection
+
+        if root_id and version:
+            bc = BranchCollection(self.project_id, self.session)
+            branch = bc.get(root_id=root_id, version=version)
+            return branch.uid
+        return None
 
     def register(self, model: DesignWorkflow) -> DesignWorkflow:
         """
@@ -42,10 +67,10 @@ class DesignWorkflowCollection(Collection[DesignWorkflow]):
             The newly created design workflow.
 
         """
-        if self.branch_id is None:
+        if self.branch_root_id is None or self.branch_version is None:
             # There are a number of contexts in which hitting design workflow endpoints without a
             # branch ID is valid, so only this particular usage is deprecated.
-            msg = ('A design workflow must be created with a branch. Please use'
+            msg = ('A design workflow must be created with a branch. Please use '
                    'branch.design_workflows.register() instead of '
                    'project.design_workflows.register().')
             raise RuntimeError(msg)
@@ -57,7 +82,8 @@ class DesignWorkflowCollection(Collection[DesignWorkflow]):
             # To avoid modifying the parameter, and to ensure the only change is the branch_id, we
             # deepcopy, modify, then register it.
             model_copy = deepcopy(model)
-            model_copy.branch_id = self.branch_id
+            model_copy._branch_id = self._resolve_branch_id(self.branch_root_id,
+                                                            self.branch_version)
             return super().register(model_copy)
 
     def build(self, data: dict) -> DesignWorkflow:
@@ -76,6 +102,7 @@ class DesignWorkflowCollection(Collection[DesignWorkflow]):
 
         """
         workflow = DesignWorkflow.build(data)
+        workflow = self._resolve_branch_root_and_version(workflow)
         workflow._session = self.session
         workflow.project_id = self.project_id
         return workflow
@@ -98,13 +125,22 @@ class DesignWorkflowCollection(Collection[DesignWorkflow]):
             The design workflow resulting from the update.
 
         """
-        if self.branch_id is not None:
-            if self.branch_id != model.branch_id:
+        if self.branch_root_id is not None or self.branch_version is not None:
+            if self.branch_root_id != model.branch_root_id or \
+                    self.branch_version != model.branch_version:
                 raise ValueError('To move a design workflow to another branch, please use '
                                  'Project.design_workflows.update')
 
-        if model.branch_id is None:
-            raise ValueError('Cannot update a design workflow unless its branch_id is set.')
+        if model.branch_root_id is not None and model.branch_version is not None:
+            try:
+                model._branch_id = self._resolve_branch_id(model.branch_root_id,
+                                                           model.branch_version)
+            except NotFound:
+                raise ValueError('Cannot update a design workflow unless its branch_root_id and '
+                                 'branch_version exists.')
+        elif model._branch_id is None:
+            raise ValueError('Cannot update a design workflow unless its branch_root_id and '
+                             'branch_version are set.')
 
         # If executions have already been done, warn about future behavior change
         executions = model.design_executions.list()
@@ -159,7 +195,7 @@ class DesignWorkflowCollection(Collection[DesignWorkflow]):
                     additional_params: Optional[dict] = None,
                     ) -> Tuple[Iterable[dict], str]:
         params = additional_params or {}
-        params["branch"] = self.branch_id
+        params["branch"] = self._resolve_branch_id(self.branch_root_id, self.branch_version)
         return super()._fetch_page(path=path,
                                    fetch_func=fetch_func,
                                    page=page,
