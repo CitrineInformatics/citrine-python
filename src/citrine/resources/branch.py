88287cc83a7643bc8ad187af573f2ecdc780ffd3
Austin Noto-Moniz <anoto-moniz@citrine.io>
2023-12-15 14:16:07 -0500

    [PLA-12982] Branches use root ID and version number.
    The version ID, a unique UUID assigned to each version number, is
    confusing to users, especially since it's never exposed in the UI. As
    such, the branch collection should prefer identifying branch versions
    via the root ID and version number. Any uses of the version ID should
    emit a deprecation warning.

--- src/citrine/resources/branch.py
+++ src/citrine/resources/branch.py
@@ -1,16 +1,23 @@
 import functools
+import warnings
 from typing import Iterator, Optional, Union
 from uuid import UUID
+
+from deprecation import deprecated
 
 from citrine._rest.collection import Collection
 from citrine._rest.resource import Resource
 from citrine._serialization import properties
 from citrine._session import Session
+from citrine._utils.functions import migrate_deprecated_argument
 from citrine.exceptions import NotFound
 from citrine.resources.data_version_update import BranchDataUpdate, NextBranchVersionRequest
 from citrine.resources.design_workflow import DesignWorkflowCollection
 from citrine.resources.experiment_datasource import (ExperimentDataSourceCollection,
                                                      ExperimentDataSource)
+
+
+LATEST_VER = "latest"  # Refers to the most recently created branch version.
 
 
 class Branch(Resource['Branch']):
@@ -50,7 +57,8 @@ class Branch(Resource['Branch']):
             raise AttributeError('Cannot initialize workflow without project reference!')
         return DesignWorkflowCollection(project_id=self.project_id,
                                         session=self.session,
-                                        branch_id=self.uid)
+                                        branch_root_id=self.root_id,
+                                        branch_version=self.version)
 
     @property
     def experiment_datasource(self) -> Optional[ExperimentDataSource]:
@@ -58,7 +66,7 @@ class Branch(Resource['Branch']):
         if getattr(self, 'project_id', None) is None:
             raise AttributeError('Cannot retrieve datasource without project reference!')
         erds = ExperimentDataSourceCollection(project_id=self.project_id, session=self.session)
-        branch_erds_iter = erds.list(branch_id=self.uid, version="latest")
+        branch_erds_iter = erds.list(root_id=self.uid, version=LATEST_VER)
         return next(branch_erds_iter, None)
 
     def _post_dump(self, data: dict) -> dict:
@@ -100,38 +108,109 @@ class BranchCollection(Collection[Branch]):
         branch.project_id = self.project_id
         return branch
 
-    def get_by_root_id(self, *, branch_root_id: Union[UUID, str]) -> Branch:
+    @deprecated(deprecated_in="2.42.0", removed_in="3.0.0", details="Use .get() instead.")
+    def get_by_root_id(self,
+                       *,
+                       branch_root_id: Union[UUID, str],
+                       version: Optional[Union[int, str]] = LATEST_VER) -> Branch:
         """
-        Given a branch root ID, retrieve the latest version of the branch.
+        Given a branch root ID and (optionally) the version, retrieve the specific branch version.
 
         Parameters
         ---------
-        branch_root_id:  Union[UUID, str]
+        branch_root_id: Union[UUID, str]
             Unique identifier of the branch root
+
+        version: Union[int, str], optional
+            The version of the branch to retrieve. If provided, must either be a positive integer,
+            or "latest". Defaults to "latest".
 
         Returns
         -------
         Branch
-            The latest version of the branch.
+            The requested version of the branch.
 
         """
-        params = {"root": str(branch_root_id), "version": "latest"}
-        branch = next(self._list_with_params(per_page=1, **params), None)
-        if branch:
-            return branch
+        return self.get(root_id=branch_root_id, version=version)
+
+    def get(self,
+            uid: Optional[Union[UUID, str]] = None,
+            *,
+            root_id: Optional[Union[UUID, str]] = None,
+            version: Optional[Union[int, str]] = LATEST_VER) -> Branch:
+        """
+        Retrieve a branch using either the version ID, or the root ID and version number.
+
+        Providing both the version ID and the root ID, or neither, will result in an error.
+
+        Providing the root ID and no version number will retrieve the latest version.
+
+        Using the version ID with this method is deprecated in favor of .get_by_version_id().
+
+        Parameters
+        ---------
+        uid: Union[UUID, str], optional
+            [deprecated] Unqiue ID of the branch version.
+
+        root_id: Union[UUID, str], optional
+            Unique identifier of the branch root
+
+        version: Union[int, str], optional
+            The version of the branch to retrieve. If provided, must either be a positive integer,
+            or "latest". Defaults to "latest".
+
+        Returns
+        -------
+        Branch
+            The requested version of the branch.
+
+        """
+        if uid:
+            warnings.warn("Retrieving a branch by its version ID using .get() is deprecated. "
+                          "Please use .get_by_version_id().",
+                          DeprecationWarning)
+
+            if root_id:
+                raise ValueError("Please provide exactly one: the version ID or root ID.")
+            return self.get_by_version_id(version_id=uid)
+        elif root_id:
+            params = {"root": str(root_id), "version": version or LATEST_VER}
+            branch = next(self._list_with_params(per_page=1, **params), None)
+            if branch:
+                return branch
+            else:
+                raise NotFound.build(
+                    message=f"Branch root '{root_id}', version {version} not found",
+                    method="GET",
+                    path=self._get_path(),
+                    params=params
+                )
+
         else:
-            raise NotFound.build(
-                message=f"Branch root '{branch_root_id}' not found",
-                method="GET",
-                path=self._get_path(),
-                params=params
-            )
+            raise ValueError("Please provide exactly one: the version ID or root ID.")
+
+    def get_by_version_id(self, *, version_id: Union[UUID, str]) -> Branch:
+        """
+        Given a branch version ID, retrieve the branch.
+
+        Parameters
+        ---------
+        version_id: Union[UUID, str]
+            Unique ID of the branch version
+
+        Returns
+        -------
+        Branch
+            The requested branch.
+
+        """
+        return super().get(version_id)
 
     def list(self, *, per_page: int = 20) -> Iterator[Branch]:
         """
-        List active branches using pagination.
+        List all branches using pagination.
 
-        Yields all active branches, paginating over all available pages.
+        Yields all branches, regardless of archive status, paginating over all available pages.
 
         Parameters
         ---------
@@ -142,9 +221,12 @@ class BranchCollection(Collection[Branch]):
         Returns
         -------
         Iterator[Branch]
-            Active branches in this collection.
+            All branches in this collection.
 
         """
+        warnings.warn("Beginning in the 3.0 release, this method will only list unarchived "
+                      "branches. To list all branches, use .list_all().",
+                      DeprecationWarning)
         return super().list(per_page=per_page)
 
     def list_archived(self, *, per_page: int = 20) -> Iterator[Branch]:
@@ -167,43 +249,109 @@ class BranchCollection(Collection[Branch]):
         """
         return self._list_with_params(per_page=per_page, archived=True)
 
+    def list_all(self, *, per_page: int = 20) -> Iterator[Branch]:
+        """
+        List active branches using pagination.
+
+        Yields all active branches, paginating over all available pages.
+
+        Parameters
+        ---------
+        per_page: int, optional
+            Max number of branches to return per page. Default is 20. This parameter is used when
+            making requests to the backend service.
+
+        Returns
+        -------
+        Iterator[Branch]
+            Active branches in this collection.
+
+        """
+        return self._list_with_params(per_page=per_page)
+
     def _list_with_params(self, *, per_page, **kwargs):
         fetcher = functools.partial(self._fetch_page, additional_params=kwargs)
         return self._paginator.paginate(page_fetcher=fetcher,
                                         collection_builder=self._build_collection_elements,
                                         per_page=per_page)
 
-    def archive(self, uid: Union[UUID, str] = None):
+    def archive(self,
+                uid: Optional[Union[UUID, str]] = None,
+                *,
+                root_id: Optional[Union[UUID, str]] = None,
+                version: Optional[int] = None):
         """
         Archive a branch.
 
         Parameters
         ----------
-        uid: Union[UUID, str]
-            Unique identifier of the branch to archive
+        uid: Union[UUID, str], optional
+            [deprecated] Unique identifier of the branch
+
+        root_id: Union[UUID, str], optional
+            Unique ID of the branch root
+
+        version: int, optional
+            The version of the branch. If provided, must be a positive integer.
 
         """
+        if uid:
+            warnings.warn("Archiving a branch by its version ID is deprecated. "
+                          "Please use its root ID and version number.",
+                          DeprecationWarning)
+        elif root_id:
+            if not version or isinstance(version, str):
+                raise ValueError("Must provide a version number to archive a branch.")
+            # The backend API at present expects a branch version ID, so we must look it up.
+            uid = self.get(root_id=root_id, version=version).uid
+        else:
+            raise ValueError("Please provide exactly one: the version ID or root ID.")
+
         url = self._get_path(uid, action="archive")
         data = self.session.put_resource(url, {}, version=self._api_version)
         return self.build(data)
 
-    def restore(self, uid: Union[UUID, str] = None):
+    def restore(self,
+                uid: Optional[Union[UUID, str]] = None,
+                *,
+                root_id: Optional[Union[UUID, str]] = None,
+                version: Optional[int] = None):
         """
         Restore an archived branch.
 
         Parameters
         ----------
-        uid: Union[UUID, str]
-            Unique identifier of the branch to restore
+        uid: Union[UUID, str], optional
+            [deprecated] Unique identifier of the branch
+
+        root_id: Union[UUID, str], optional
+            Unique ID of the branch root
+
+        version: int, optional
+            The version of the branch. If provided, must be a positive integer.
 
         """
+        if uid:
+            warnings.warn("Restoring a branch by its version ID is deprecated. "
+                          "Please use its root ID and version number.",
+                          DeprecationWarning)
+        elif root_id:
+            if not version or isinstance(version, str):
+                raise ValueError("Must provide a version number to restore a branch.")
+            # The backend API at present expects a branch version ID, so we must look it up.
+            uid = self.get(root_id=root_id, version=version).uid
+        else:
+            raise ValueError("Please provide exactly one: the version ID or root ID.")
+
         url = self._get_path(uid, action="restore")
         data = self.session.put_resource(url, {}, version=self._api_version)
         return self.build(data)
 
     def update_data(self,
-                    branch: Union[UUID, str, Branch],
+                    branch: Optional[Union[UUID, str, Branch]] = None,
                     *,
+                    root_id: Optional[Union[UUID, str]] = None,
+                    version: Optional[int] = None,
                     use_existing: bool = True,
                     retrain_models: bool = False) -> Optional[Branch]:
         """
@@ -214,8 +362,14 @@ class BranchCollection(Collection[Branch]):
 
         Parameters
         ----------
-        branch: Union[UUID, str, Branch]
-            Branch Identifier or Branch object
+        branch: Union[UUID, str, Branch], optional
+            [deprecated] Branch Identifier or Branch object
+
+        root_id: Union[UUID, str], optional
+            Unique ID of the branch root
+
+        version: int, optional
+            The version of the branch. If provided, must be a positive integer.
 
         use_existing: bool
             If true the workflows in this branch will use existing predictors that are using
@@ -231,9 +385,25 @@ class BranchCollection(Collection[Branch]):
             The new branch record after version update or None if no update
 
         """
-        if not isinstance(branch, Branch):
-            branch = self.get(branch)
-        version_updates = self.data_updates(branch.uid)
+        if branch:
+            if root_id:
+                raise ValueError("Please provide exactly one: the version ID or root ID.")
+
+            if isinstance(branch, Branch):
+                warnings.warn("Updating a branch's data by its object is deprecated. "
+                              "Please use its root ID and version number.",
+                              DeprecationWarning)
+            else:
+                warnings.warn("Updating a branch's data by its version ID is deprecated. "
+                              "Please use its root ID and version number.",
+                              DeprecationWarning)
+                branch = self.get_by_version_id(version_id=branch)
+            root_id = branch.root_id
+            version = branch.version
+        elif not root_id:
+            raise ValueError("Please provide exactly one: the version ID or root ID.")
+
+        version_updates = self.data_updates(root_id=root_id, version=version)
         # If no new data sources, then exit, nothing to do
         if len(version_updates.data_updates) == 0:
             return None
@@ -244,19 +414,29 @@ class BranchCollection(Collection[Branch]):
 
         branch_instructions = NextBranchVersionRequest(data_updates=version_updates.data_updates,
                                                        use_predictors=use_predictors)
-        branch = self.next_version(branch.root_id,
+        branch = self.next_version(root_id=root_id,
                                    branch_instructions=branch_instructions,
                                    retrain_models=retrain_models)
         return branch
 
-    def data_updates(self, uid: Union[UUID, str]) -> BranchDataUpdate:
+    def data_updates(self,
+                     uid: Optional[Union[UUID, str]] = None,
+                     *,
+                     root_id: Optional[Union[UUID, str]] = None,
+                     version: Optional[int] = None) -> BranchDataUpdate:
         """
         Get data updates for a branch.
 
         Parameters
         ----------
-        uid:  Union[UUID, str]
-            Unique identifier of the branch to get data updates for
+        uid: Union[UUID, str], optional
+            [deprecated] Unqiue ID of the branch version.
+
+        root_id: Union[UUID, str], optional
+            Unique ID of the branch root
+
+        version: int, optional
+            The version of the branch. If provided, must be a positive integer.
 
         Returns
         -------
@@ -264,12 +444,27 @@ class BranchCollection(Collection[Branch]):
             A list of data updates and compatible predictors
 
         """
+        if uid:
+            warnings.warn("Retrieving data updates for a branch by its version ID is deprecated. "
+                          "Please use its root ID and version number.",
+                          DeprecationWarning)
+            if root_id:
+                raise ValueError("Please provide exactly one: the version ID or root ID.")
+        elif root_id:
+            if version is None or isinstance(version, str):
+                raise ValueError("Must provide a version number to retrieve data updates.")
+            # The backend API at present expects a branch version ID, so we must look it up.
+            uid = self.get(root_id=root_id, version=version).uid
+        else:
+            raise ValueError("Please provide exactly one: the version ID or root ID.")
+
         path = self._get_path(uid=uid, action="data-version-updates-predictor")
         data = self.session.get_resource(path, version=self._api_version)
         return BranchDataUpdate.build(data)
 
     def next_version(self,
-                     branch_root_id: Union[UUID, str],
+                     branch_root_id: Optional[Union[UUID, str]] = None,
+                     root_id: Optional[Union[UUID, str]] = None,
                      *,
                      branch_instructions: NextBranchVersionRequest,
                      retrain_models: bool = True):
@@ -278,7 +473,11 @@ class BranchCollection(Collection[Branch]):
 
         Parameters
         ----------
-        branch_root_id:  Union[UUID, str]
+        branch_root_id: Union[UUID, str], optional
+            [deprecated] Unique identifier of the branch root to advance to next version.
+            Deprecated in favor of root_id.
+
+        root_id: Union[UUID, str], optional
             Unique identifier of the branch root to advance to next version
 
         branch_instructions: NextBranchVersionRequest
@@ -299,10 +498,12 @@ class BranchCollection(Collection[Branch]):
             The new branch record after version update
 
         """
+        root_id = migrate_deprecated_argument(root_id, "root_id", branch_root_id, "branch_root_id")
+
         path = self._get_path(action="next-version-predictor")
         data = self.session.post_resource(path, branch_instructions.dump(),
                                           version=self._api_version,
                                           params={
-                                              'root': str(branch_root_id),
+                                              'root': str(root_id),
                                               'retrain_models': retrain_models})
         return self.build(data)
