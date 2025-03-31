6a48dd3fe62e474dfd87b6d7cac01d2bd04ded32
Jamie Heller <jheller@citrine.io>
2025-03-31 16:41:34 -0400

    PNE-6482 Support `default_labels` in MaterialSpec constructor
    This update is analagous to the one in https://github.com/CitrineInformatics/citrine-python/pull/989
    
    The associated utility function and test have been moved to a shared module,
    and the documentation has been updated

---
+++ src/citrine/resources/_default_labels.py
@@ -0,0 +1,21 @@
+from typing import List, Optional
+
+from citrine.resources.data_concepts import CITRINE_TAG_PREFIX
+
+_CITRINE_DEFAULT_LABEL_PREFIX = f'{CITRINE_TAG_PREFIX}::mat_label'
+
+
+def _inject_default_label_tags(
+    original_tags: Optional[List[str]], default_labels: Optional[List[str]]
+) -> Optional[List[str]]:
+    if default_labels is None:
+        all_tags = original_tags
+    else:
+        labels_as_tags = [
+            f"{_CITRINE_DEFAULT_LABEL_PREFIX}::{label}" for label in default_labels
+        ]
+        if original_tags is None:
+            all_tags = labels_as_tags
+        else:
+            all_tags = list(original_tags) + labels_as_tags
+    return all_tags
