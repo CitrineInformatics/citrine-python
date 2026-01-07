52fa993e70f869cdf80a38eb061f785c6c657067
Austin Noto-Moniz <anoto-moniz@citrine.io>
2026-01-07 14:27:14 -0500

    Drop support for CSVDataSource

--- tests/informatics/test_data_source.py
+++ tests/informatics/test_data_source.py
@@ -4,7 +4,7 @@ import uuid
 import pytest
 
 from citrine.informatics.data_sources import (
-    DataSource, CSVDataSource, ExperimentDataSourceRef, GemTableDataSource, SnapshotDataSource
+    DataSource, ExperimentDataSourceRef, GemTableDataSource, SnapshotDataSource
 )
 from citrine.informatics.descriptors import RealDescriptor
 from citrine.resources.file_link import FileLink
@@ -54,27 +54,3 @@ def test_from_gem_table():
 def test_invalid_data_source_id():
     with pytest.raises(ValueError):
         DataSource.from_data_source_id(f"Undefined::{uuid.uuid4()}")
-
-
-def test_deser_from_parent_deprecated():
-    with pytest.deprecated_call():
-        data_source = CSVDataSource(file_link=FileLink("foo.spam", "http://example.com"),
-                                    column_definitions={"spam": RealDescriptor("eggs", lower_bound=0, upper_bound=1.0, units="")},
-                                    identifiers=["identifier"])
-
-    # Serialize and deserialize the descriptors, making sure they are round-trip serializable
-    data = data_source.dump()
-    data_source_deserialized = DataSource.build(data)
-    assert data_source == data_source_deserialized
-
-def test_data_source_id_deprecated():
-    with pytest.deprecated_call():
-        data_source = CSVDataSource(file_link=FileLink("foo.spam", "http://example.com"),
-                                    column_definitions={"spam": RealDescriptor("eggs", lower_bound=0, upper_bound=1.0, units="")},
-                                    identifiers=["identifier"])
-    
-    # TODO: There's no obvious way to recover the column_definitions & identifiers from the ID
-    with pytest.deprecated_call():
-        with pytest.warns(UserWarning):
-            transformed = DataSource.from_data_source_id(data_source.to_data_source_id())
-    assert transformed.file_link == data_source.file_link
